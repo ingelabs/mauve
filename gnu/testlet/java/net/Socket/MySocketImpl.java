82131a2268e6ba5e62cf43b517d77c3070f6a809
Warren Levy <warrenl@redhat.com>
1999-04-29 15:05:24 +0000

    * gnu/testlet/java/net/InetAddress/InetAddressTest.java: New file. 	* gnu/testlet/java/net/ServerSocket/BasicBacklogSocketServer.java: 	  New file. 	* gnu/testlet/java/net/ServerSocket/BasicSocketServer.java: New file. 	* gnu/testlet/java/net/ServerSocket/MyBasicSocketServer.java: New file. 	* gnu/testlet/java/net/ServerSocket/MyServerSocket.java: New file. 	* gnu/testlet/java/net/ServerSocket/ServerSocketTest.java: New file. 	* gnu/testlet/java/net/Socket/MySocket.java: New file. 	* gnu/testlet/java/net/Socket/MySocketImpl.java: New file. 	* gnu/testlet/java/net/Socket/SocketBServer.java: New file. 	* gnu/testlet/java/net/Socket/SocketServer.java: New file. 	* gnu/testlet/java/net/Socket/SocketTest.java: New file. 	* gnu/testlet/java/net/URL/MyURLStreamHandler.java: New file. 	* gnu/testlet/java/net/URL/URLTest.java: New file. 	* gnu/testlet/java/net/URLConnection/MyHttpURLConnection.java: 	  New file. 	* gnu/testlet/java/net/URLConnection/MyURLConnection.java: New file. 	* gnu/testlet/java/net/URLConnection/URLConnectionTest.java: New file. 	* gnu/testlet/java/net/URLEncoder/URLEncoderTest.java: New file.

---
+++ gnu/testlet/java/net/Socket/MySocketImpl.java
@@ -0,0 +1,45 @@
+// Tags: not-a-test
+
+/*
+   Copyright (C) 1999 Hewlett-Packard Company
+
+   This file is part of Mauve.
+
+   Mauve is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   Mauve is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Mauve; see the file COPYING.  If not, write to
+   the Free Software Foundation, 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+package gnu.testlet.java.net.Socket;
+import gnu.testlet.Testlet;
+import gnu.testlet.TestHarness;
+import java.net.*;
+import java.io.*;
+
+
+class MySocketImpl extends SocketImpl {
+    public MySocketImpl() {}
+    protected void create(boolean stream) throws IOException {}
+    protected void connect(String host, int port) throws IOException {}
+    protected void connect(InetAddress address, int port) throws IOException {}
+    protected void bind(InetAddress host, int port) throws IOException {}
+    protected void listen(int backlog) throws IOException {}
+    protected void accept(SocketImpl s) throws IOException {}
+    protected InputStream getInputStream() throws IOException {return null;}
+    protected OutputStream getOutputStream() throws IOException { return null; }
+    protected int available() throws IOException { return 0; }
+    protected void close() throws IOException {}
+  public void setOption(int optID, Object value) throws SocketException {}
+  public Object getOption(int optID) throws SocketException { return null; }
+}
