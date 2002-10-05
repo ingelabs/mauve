492afd19288b21b080d3281a5c31e83459e58214
Mark Wielaard <mark@klomp.org>
2002-10-05 15:22:05 +0000

    * gnu/testlet/java/lang/Thread/interrupt.java: New Test.         * gnu/testlet/java/lang/Thread/name.java: New Test.         * gnu/testlet/java/lang/Thread/daemon.java: New Test.         * gnu/testlet/java/lang/Thread/priority.java: New Test.         * gnu/testlet/java/lang/Thread/isAlive.java: New Test.

---
+++ gnu/testlet/java/lang/Thread/daemon.java
@@ -0,0 +1,94 @@
+// Tags: JDK1.0
+
+// Copyright (C) 2002 Free Software Foundation, Inc.
+// Written by Mark Wielaard (mark@klomp.org)
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.  */
+
+package gnu.testlet.java.lang.Thread;
+
+import gnu.testlet.Testlet;
+import gnu.testlet.TestHarness;
+
+public class daemon extends Thread implements Testlet
+{
+
+  boolean started = false;
+  public void run()
+  {
+    synchronized(this)
+      {
+	started = true;
+	notify();
+      }
+  }
+
+  public void test (TestHarness harness)
+  {
+    Thread current = Thread.currentThread();
+
+    boolean status = current.isDaemon();
+    boolean illegal_exception = false;
+    try
+      {
+	current.setDaemon(!status);
+      }
+    catch (IllegalThreadStateException itse)
+      {
+	illegal_exception = true;
+      }
+    harness.check(illegal_exception,
+		    "Cannot change daemon state on current Thread");
+    harness.check(current.isDaemon() == status,
+		    "Daemon status does not change when set on current Thread");
+
+    daemon t  = new daemon();
+    harness.check(t.isDaemon() == status,
+		    "Newly created thread gets daemon status of creator");
+
+    t.setDaemon(!status);
+    harness.check(t.isDaemon() != status,
+		    "Can change daemon status on unstarted Thread");
+
+    t.start();
+    synchronized(t)
+      {
+	while (!t.started)
+	  try { t.wait(); } catch(InterruptedException ignored) { }
+      }
+
+    harness.check(t.isDaemon() != status,
+		    "Daemon status does not change when starting Thread");
+
+    illegal_exception = false;
+    try
+      {
+	t.setDaemon(!status);
+      }
+    catch (IllegalThreadStateException itse)
+      {
+	illegal_exception = true;
+      }
+    harness.check(illegal_exception,
+		    "Cannot change daemon state on started Thread");
+    harness.check(t.isDaemon() != status,
+		    "Daemon status does not change when set on started Thread");
+
+  }
+}
+
