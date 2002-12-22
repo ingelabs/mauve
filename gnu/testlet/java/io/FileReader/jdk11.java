d51dab8a5bddc40e9527f47ecff31937fd284e6d
Daryl Lee <dol@sources.redhat.com>
2002-12-22 22:16:40 +0000

    Added gnu/testlet/java/io/FileReader/jdk11.java to complete constructor tests for this class

---
+++ gnu/testlet/java/io/FileReader/jdk11.java
@@ -0,0 +1,76 @@
+/*************************************************************************
+/* jdk11.java -- java.io.FileReader 1.1 tests
+/*
+/* Copyright (c) 2001, 2002 Free Software Foundation, Inc.
+/*
+/* This program is free software; you can redistribute it and/or modify
+/* it under the terms of the GNU General Public License as published 
+/* by the Free Software Foundation, either version 2 of the License, or
+/* (at your option) any later version.
+/*
+/* This program is distributed in the hope that it will be useful, but
+/* WITHOUT ANY WARRANTY; without even the implied warranty of
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/* GNU General Public License for more details.
+/*
+/* You should have received a copy of the GNU General Public License
+/* along with this program; if not, write to the Free Software Foundation
+/* Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+/*************************************************************************/
+
+// Tags: JDK1.1
+
+package gnu.testlet.java.io.FileReader;
+
+import gnu.testlet.Testlet;
+import gnu.testlet.TestHarness;
+import gnu.testlet.SimpleTestHarness;
+import java.io.FileDescriptor;
+import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class jdk11 implements Testlet
+{
+  
+  public void test (TestHarness testharness)
+  {
+    SimpleTestHarness harness = null;
+    try 
+      {
+		harness = (SimpleTestHarness)testharness;
+      } 
+    catch (ClassCastException cce)
+      {
+		harness.fail ("Harness not an instance of SimpleTestHarness");
+		return;
+      }
+    try {
+      FileReader fr1 = new FileReader("choices");
+	  harness.check(true, "FileReader(string)");
+    }
+    catch (FileNotFoundException e) {
+      harness.fail("Can't open file 'choices'");
+    } 
+    try {
+	  File f2 = new File("choices");
+      FileReader fr2 = new FileReader(f2);
+	  harness.check(true, "FileReader(File)");
+	  FileInputStream fis = new FileInputStream(f2);
+      try {
+	    FileReader fr3 = new FileReader(fis.getFD());
+	    harness.check(true, "FileReader(FileDescriptor)");
+      }
+      catch (IOException e) {
+		harness.fail("Couldn't get FileDescriptor)");
+      }
+    }
+    catch (FileNotFoundException e) {
+      harness.fail("Can't open file 'choices'");
+    } 
+ }
+}
+
+
