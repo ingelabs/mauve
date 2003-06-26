c19ec520b2044121fe82462749d6cfbedeee654b
Stephen Crawley <crawley@dstc.edu.au>
2003-06-26 16:20:21 +0000

    Added lower/uppercase letter test for java/text/CollationElementIterator

--- gnu/testlet/java/text/CollationElementIterator/jdk11.java
+++ gnu/testlet/java/text/CollationElementIterator/jdk11.java
@@ -34,69 +34,10 @@ public class jdk11 implements Testlet
   private static final int PRIMARY = 1;
   private static final int SECONDARY = 2;
   private static final int TERTIARY = 3;
+  private static final int NONE = 4;
 
   private static TestHarness harness = null;
 
-  public void test(TestHarness harness)
-  {
-    // FIXME ... add more test strings for the en_US locale
-    // FIXME ... add tests for characters that compare equal
-    // FIXME ... add tests for other locales
-    final String[] TEST_STRINGS = {
-      "X",
-      "12",
-      "abcdefghijklmnopqrstuvwxyz",
-      "0123456789",
-      " _,;:!?/.`^~'\"()[]{}@$*\\&#%+<=>|",
-      "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ",
-    };
-    final int[] TEST_ORDERS = {
-      PRIMARY,
-      PRIMARY,
-      PRIMARY, 
-      PRIMARY,
-      PRIMARY,
-      TERTIARY,
-    };
-    
-    this.harness = harness;
-    try
-    {
-      // -------- constants -------- 
-      harness.check(CollationElementIterator.NULLORDER, -1, "NULLORDER");
-  
-      RuleBasedCollator en_USCollator = (RuleBasedCollator) 
-	Collator.getInstance(new Locale("en", "US", ""));
-      CollationElementIterator iterator = 
-        en_USCollator.getCollationElementIterator("abcdefg");
-
-      // -------- methods -------- 
-      checkOrder(iterator, 7, PRIMARY, "en_US");
-
-      // reset()
-      harness.checkPoint("reset()");
-      iterator.reset();
-      checkOrder(iterator, 7, PRIMARY, "en_US");
-
-      // ------- check empty string --------
-      iterator = en_USCollator.getCollationElementIterator("");
-      harness.check (iterator.next(), CollationElementIterator.NULLORDER, 
-		     "next()");
-
-      // ------- detailed checks of collation orders -------
-      for (int i = 0; i < TEST_STRINGS.length; i++) {
-	iterator = en_USCollator.getCollationElementIterator(TEST_STRINGS[i]);
-	checkOrder(iterator, TEST_STRINGS[i].length(), TEST_ORDERS[i], 
-		   "test string #" + i);
-      }
-    } 
-    catch (Throwable t)
-    {
-      harness.debug(t);
-      harness.fail("CollationElementIterator");
-    }
-  }
- 
   static void checkOrder (CollationElementIterator iterator, 
 			  int count, int order, String tag)
   {
@@ -115,6 +56,7 @@ public class jdk11 implements Testlet
       
       int i = 1;
       while ((key = iterator.next()) != CollationElementIterator.NULLORDER) {
+        i++;
 	int[] next = {key, 
 		      CollationElementIterator.primaryOrder(key),
 		      CollationElementIterator.secondaryOrder(key),
@@ -144,7 +86,6 @@ public class jdk11 implements Testlet
 			"no tertiary difference" + i + " " + tag);
 	}	
 	prev = next;
-        i++;
       }
       if (count != i) {
 	harness.debug("count is " + count + ", i is " + i);
@@ -157,6 +98,148 @@ public class jdk11 implements Testlet
       harness.fail ("CollationElementIterator with localeName");
     }
   }
+
+
+  static void checkEquiv (CollationElementIterator iterator, 
+			  String[] sets, int order, String tag)
+  {
+    try
+    {
+      for (int i = 0; i < sets.length; i++) {
+	iterator.setText(sets[i]);
+	int key = iterator.next();
+	int[] prev = {key, 
+		      CollationElementIterator.primaryOrder(key),
+		      CollationElementIterator.secondaryOrder(key),
+		      CollationElementIterator.tertiaryOrder(key)
+	};
+	harness.debug("first = {" + prev[0] + ", " + prev[1] + ", " +
+		      prev[2] + ", " + prev[3] + "}");
+	harness.check(key != CollationElementIterator.NULLORDER, 
+		      "first " + tag);
+	
+	int j = 1;
+	while ((key = iterator.next()) != CollationElementIterator.NULLORDER) {
+	  j++;
+	  int[] next = {key, 
+			CollationElementIterator.primaryOrder(key),
+			CollationElementIterator.secondaryOrder(key),
+			CollationElementIterator.tertiaryOrder(key)
+	  };
+	  
+	  harness.debug("next (" + i + ", " + j + ") = {" + 
+			next[0] + ", " + next[1] + ", " +
+			next[2] + ", " + next[3] + "}");
+	  
+	  if (order == PRIMARY) {
+	    harness.check(next[1] > prev[1],
+			  "not primary ordered " + i + ", " + j + " " + tag);
+	  }
+	  else if (order == SECONDARY) {
+	    harness.check(next[1] == prev[1] && next[2] > prev[2],
+			  "not secondary ordered" + i + ", " + j + " " + tag);
+	  }
+	  else if (order == TERTIARY) {
+	    harness.check(next[1] == prev[1] && next[2] == prev[2] && 
+			  next[3] > prev[3],
+			  "not tertiary ordered" + i + ", " + j + " " + tag);
+	  }
+	  else if (order == NONE) {
+	    harness.check(next[1] == prev[1], "keys not equal");
+	  }
+	  prev = next;
+	}
+	if (sets[i].length() != j) {
+	  harness.debug("length[" + i + "] is " + sets[i].length() +
+			", j is " + j);
+	}
+	harness.check(sets[i].length() == j, 
+		      "wrong number of keys (" + j + ") " + tag);
+      }
+    } 
+    catch (Throwable t)
+      {
+	harness.debug (t);
+	harness.fail ("CollationElementIterator with localeName");
+      }
+  }
+
+
+  public void test(TestHarness harness)
+  {
+    // FIXME ... add more test strings for the en_US locale
+    // FIXME ... add tests for characters that compare equal
+    // FIXME ... add tests for other locales
+    final String[] TEST_STRINGS = {
+      "X",
+      "12",
+      "abcdefghijklmnopqrstuvwxyz",
+      "0123456789",
+      " _,;:!?/.`^~'\"()[]{}@$*\\&#%+<=>|",
+      "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ",
+    };
+    final int[] TEST_ORDERS = {
+      PRIMARY,
+      PRIMARY,
+      PRIMARY, 
+      PRIMARY,
+      PRIMARY,
+      TERTIARY,
+    };
+
+    final String[][] TEST_STRINGS2 = {
+      {"aA", "bB", "cC", "dD", "eE", "fF", "gG", "hH", "iI", "jJ", "kK", 
+       "lL", "mM", "nN", "oO", "pP", "qQ", "rR", "sS", "tT", "uU", "vV", 
+       "wW", "xX", "yY", "zZ"},
+    };
+    final int[] TEST_ORDERS2 = {
+      TERTIARY,
+    };
+    
+    this.harness = harness;
+    try
+    {
+      // -------- constants -------- 
+      harness.check(CollationElementIterator.NULLORDER, -1, "NULLORDER");
+  
+      RuleBasedCollator en_USCollator = (RuleBasedCollator) 
+	Collator.getInstance(new Locale("en", "US", ""));
+      CollationElementIterator iterator = 
+        en_USCollator.getCollationElementIterator("abcdefg");
+
+      // -------- methods -------- 
+      checkOrder(iterator, 7, PRIMARY, "initial test");
+
+      // reset()
+      harness.checkPoint("reset()");
+      iterator.reset();
+      checkOrder(iterator, 7, PRIMARY, "initial test after reset()");
+
+      // ------- check empty string --------
+      iterator = en_USCollator.getCollationElementIterator("");
+      harness.check (iterator.next(), CollationElementIterator.NULLORDER, 
+		     "next()");
+
+      // ------- detailed checks of collation orders -------
+      for (int i = 0; i < TEST_STRINGS.length; i++) {
+	iterator = en_USCollator.getCollationElementIterator(TEST_STRINGS[i]);
+	checkOrder(iterator, TEST_STRINGS[i].length(), TEST_ORDERS[i], 
+		   "test string #" + i);
+      }
+
+      // ------- detailed checks of collation equivalences -------
+      for (int i = 0; i < TEST_STRINGS2.length; i++) {
+	checkEquiv(iterator, TEST_STRINGS2[i], TEST_ORDERS2[i], 
+		   "test set #" + i);
+      }
+    } 
+    catch (Throwable t)
+    {
+      harness.debug(t);
+      harness.fail("CollationElementIterator");
+    }
+  }
+ 
 
 } // class jdk11
 
