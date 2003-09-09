abe5357f6496942455b0debfa6070c3775c4ed6d
Mark Wielaard <mark@klomp.org>
2003-09-09 22:10:22 +0000

    * gnu/testlet/java/lang/reflect/Array/set.java: Check IllegalArgument        and ArrayIndexOutOfBoundsException.

--- gnu/testlet/java/lang/reflect/Array/set.java
+++ gnu/testlet/java/lang/reflect/Array/set.java
@@ -28,8 +28,53 @@ public class set implements Testlet
 {
   public void test(TestHarness harness)
   {
-    Object[] x = new Object[2];
+    Throwable[] x = new Throwable[5];
     Array.set (x, 0, null);
     harness.check (x[0], null);
+
+    Throwable t = new Throwable();
+    Array.set (x, 1, t);
+    harness.check(x[1], t);
+
+    Exception e = new Exception();
+    Array.set (x, 2, e);
+    harness.check(x[2], e);
+
+    Object o = new Object();
+    boolean exception_thrown = false;
+    try
+      {
+	Array.set (x, 3, o);
+      }
+    catch (IllegalArgumentException iae)
+      {
+	exception_thrown = true;
+      }
+    harness.check(exception_thrown);
+    harness.check(x[3], null);
+
+    String s = "string";
+    exception_thrown = false;
+    try
+      {
+	Array.set (x, 4, s);
+      }
+    catch (IllegalArgumentException iae)
+      {
+	exception_thrown = true;
+      }
+    harness.check(exception_thrown);
+    harness.check(x[4], null);
+
+    exception_thrown = false;
+    try
+      {
+	Array.set (x, 5, t);
+      }
+    catch (ArrayIndexOutOfBoundsException aioobe)
+      {
+	exception_thrown = true;
+      }
+    harness.check(exception_thrown);
   }
 }
