5b4eaaf0285e516d09b26ed3764b0450a7c026f2
Guilhem Lavaux <guilhem@kaffe.org>
2003-12-15 20:03:34 +0000

    Added copyright advertisement.

--- gnu/testlet/java/io/ObjectInputOutput/Compat1.java
+++ gnu/testlet/java/io/ObjectInputOutput/Compat1.java
@@ -1,3 +1,24 @@
+// Tags: JDK1.2
+// Uses: SerBase
+
+/* SerTest.java -- Test class that "overrides" private field 'a'.
+
+   Copyright (c) 2003 by Free Software Foundation, Inc.
+   Written by Guilhem Lavaux (guilhem@kaffe.org).
+   Based on a test by Pat Tullmann <pat_kaffe@tullmann.org>.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation, version 2. (see COPYING)
+ 
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation
+   Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA */
 package gnu.testlet.java.io.ObjectInputOutput;
 import gnu.testlet.Testlet;
 import gnu.testlet.TestHarness;
