49b9c316996e69a0c5431ecb2db84e422ebda36b
Ito kazumitsu <kaz@maczuka.gcd.org>
2004-03-22 22:07:33 +0000

    Added tests copied from Kaffe's regression test to gnu/testlet/java/util/Properties/load.java

--- gnu/testlet/java/util/Properties/load.java
+++ gnu/testlet/java/util/Properties/load.java
@@ -59,5 +59,70 @@ public class load implements Testlet
     harness.check (in.size(), 2);
     harness.check (in.getProperty ("a space"), "a value");
     harness.check (in.getProperty ("two  spaces"), "spacy  ");
+
+    // Tests copied from Kaffe's regression test
+    p = new Properties();
+    in = new Properties();
+
+    for (int i = 0; i < EXPECT.length / 2; i++)
+      p.setProperty(EXPECT[i * 2], EXPECT[i * 2 + 1]);
+
+    try
+      {
+        in.load(new ByteArrayInputStream(INPUT.getBytes()));
+      }
+    catch (IOException _)
+      {
+      }
+    harness.check (in.size(), p.size());
+    for (int i = 0; i < EXPECT.length / 2; i++) {
+      harness.check (in.getProperty (EXPECT[i * 2]),
+                     p.getProperty (EXPECT[i * 2]));
+    }
   }
+
+  private static final String[] EXPECT = new String[] {
+          "fooKey",                       "fooValue",
+          "barKey",                       "override previous bar value",
+          "noEqualsSign",                 "value",
+          "spacesAroundEqualsSign",       "this is the value",
+          "useColon",                     "colon's value",
+          "key=contains=equals",          "value3",
+          "key:contains:colons",          "value4",
+          "key contains spaces",          "value5",
+          "backslashes",                  "\r\n\t\\\"'",
+          "lineContinuation",
+                  "this is a line continuation times two",
+          "unicodeString",
+                  new String(new char[] { (char)0x1234, (char)0x5678 } ),
+          "controlKey",
+                  new String(new char[] { (char)1, } ),
+          "keyWithEmptyStringAsValue",    ""
+  };
+
+  private static final String INPUT =
+"fooKey=fooValue\n" +
+"controlKey:\\u0001\n" +
+"barKey=bar value\n" +
+"barKey=override previous bar value\n" +
+"noEqualsSign value\n" +
+"spacesAroundEqualsSign   =  this is the value\n" +
+"useColon : colon's value\n" +
+"key\\=contains\\=equals=value3\n" +
+"key\\:contains\\:colons=value4\n" +
+"key\\ contains\\ spaces=value5\n" +
+"backslashes = \\r\\n\\t\\\\\\\"\\'\n" +
+"lineContinuation=this is a \\\n" +
+"   line continuation \\\n" +
+"   times two\n" +
+"unicodeString=\\u1234\\u5678\n" +
+"keyWithEmptyStringAsValue\n" +
+"# a few blank lines follow\n" +
+"\n" +
+"\n" +
+"# comment line\n" +
+"      ## another comment line\n" +
+"! another comment line\n" +
+"      !! another comment line\n";
+
 }
