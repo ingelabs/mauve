bbb9a62b6e5fca033e7aa841efd21281a51ccc2b
Michael Koch <konqueror@gmx.de>
2004-12-22 14:44:19 +0000

    2004-12-22  Arnaud Vandyck  <arnaud.vandyck@ulg.ac.be>
    	* gnu/testlet/javax/swing/text/StringContent/StringContentTest.java:
    	New file.

---
+++ gnu/testlet/javax/swing/text/StringContent/StringContentTest.java
@@ -0,0 +1,113 @@
+// Tags: JDK1.2
+
+// Copyright (C) 2004 Arnaud Vandyck <avdyk@gnu.org>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+package gnu.testlet.javax.swing.text.StringContent;
+
+import gnu.testlet.Testlet;
+import gnu.testlet.TestHarness;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Position;
+import javax.swing.text.StringContent;
+import javax.swing.text.Segment;
+import javax.swing.undo.UndoableEdit;
+
+public class StringContentTest implements Testlet
+{
+  public void test(TestHarness h)
+  {
+    h.checkPoint("StringContent");
+
+    StringContent sc = new StringContent();
+    try
+      {
+        h.checkPoint("StringContent -- insertString()");
+        sc.insertString(0, "path");
+        h.check("path", sc.getString(0, sc.length() -1),
+                "StringContent.insertString(): insert 'path' at 0");
+        h.checkPoint("StringContent -- length()");
+        h.check(5, sc.length(),
+                "StringContent.length(): should be 5, is: " + sc.length());
+        h.checkPoint("StringContent -- insertString() part2");
+        sc.insertString(0, "class");
+        h.checkPoint("StringContent -- getString()");
+        h.check("classpath", sc.getString(0, sc.length() -1), 
+                "StringContent.insertString(): put 'class' at 0");
+        h.checkPoint("StringContent -- length() part2");
+        h.check(10, sc.length(),
+                "StringContent.length(): should be 9, is: " + sc.length());
+        h.checkPoint("StringContent -- remove()");
+        sc.remove(1, 4);
+        h.checkPoint("StringContent -- getString() part2");
+        h.check("cpath", sc.getString(0, sc.length()-1),
+                "StringContent.remove(): should be 'cpath' is '"
+                + sc.getString(0, sc.length()-1) + "'");
+        h.checkPoint("StringContent -- remove() part2");
+        sc.remove(2, 3);
+        h.checkPoint("StringContent -- getString() part3");
+        h.check("cp", sc.getString(0, sc.length()-1),
+                "StringContent.remove(): should be 'cp' is '"
+                + sc.getString(0, sc.length()-1) + "'");
+        h.checkPoint("StringContent -- getChars()");
+        char[] ctab = { 'c', 'p' };
+        Segment s = new Segment(ctab, 0, 2);
+        Segment s2 = new Segment();
+        sc.getChars(0, sc.length() -1, s2);
+        h.check(s.toString(), s2.toString(),
+                "StringContent.getChars(): "
+                + "compare to javax.swing.text.Segment "
+                + "(first Segment: " + s + "; second Segment: " + s2 + ")");
+        h.checkPoint("StringContent -- StringContent()");
+        sc = new StringContent(100);
+        h.check(1, sc.length(), "StringContent(100): length() should be 1 and is : " + sc.length());
+        h.checkPoint("StringContent -- StringContent() part2");
+        sc = new StringContent(1);
+        h.check(1, sc.length(), "StringContent(1): length() should be 1 and is : " + sc.length());
+        h.checkPoint("StringContent -- StringContent() part3");
+        sc = new StringContent(0);
+        h.check(1, sc.length(), "StringContent(0): length() should be 1 and is : " + sc.length());
+        h.checkPoint("StringContent -- StringContent() part4");
+        sc = new StringContent();
+        h.check(1, sc.length(), "StringContent(): length() should be 1 and is : " + sc.length());
+        h.checkPoint("StringContent() -- createPosition()");
+        sc.insertString(0, "classpath");
+        Position position = sc.createPosition(1);
+        Position position2 = sc.createPosition(4);
+        h.check(1, position.getOffset(), "createPosition(1): Position.getOffset() should be 1 and is: " + position.getOffset());
+        sc.insertString(2, "-");
+        h.check(1, position.getOffset(), "Position.getOffset() should be 1 and is: " + position.getOffset());
+        sc.insertString(1, "-");
+        h.check(2, position.getOffset(), "Position.getOffset() should be 2 and is: " + position.getOffset());
+        sc.remove(0, 2);
+        h.check(0, position.getOffset(), "Position.getOffset() should be 0 and is: " + position.getOffset());
+        sc.remove(0, 5);
+        h.check(0, position.getOffset(), "Position.getOffset() should be 0 and is: " + position.getOffset());
+        h.check("path", sc.getString(0, sc.length() -1), "getString(0, length()) should be 'path' and is: " + sc.getString(0, sc.length()));
+        h.check(0, position2.getOffset(), "Position.getOffset() should be 0 and is: " + position2.getOffset());
+      }
+    catch (BadLocationException ble)
+      {
+        h.fail("BadLocation! " + ble.getMessage());
+      }
+  }
+
+}
+
