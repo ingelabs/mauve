8dba1fa328235075972c027906286fb8dbf1dc36
Michael Koch <konqueror@gmx.de>
2005-01-14 13:47:35 +0000

    2005-01-14  Arnaud Vandyck  <avdyk@gnu.org>
    	* gnu/testlet/javax/swing/text/StringContent/removeUndo.java:
    	More testcases.

--- gnu/testlet/javax/swing/text/StringContent/removeUndo.java
+++ gnu/testlet/javax/swing/text/StringContent/removeUndo.java
@@ -29,6 +29,8 @@ import javax.swing.text.Position;
 import javax.swing.text.StringContent;
 import javax.swing.text.Segment;
 import javax.swing.undo.UndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
 
 public class removeUndo implements Testlet
 {
@@ -37,6 +39,9 @@ public class removeUndo implements Testlet
     h.checkPoint("StringContent");
 
     StringContent sc = new StringContent();
+    UndoableEdit ue = null;
+    UndoableEdit ue2 = null;
+    UndoableEdit ue3 = null;
     try
       {
         h.checkPoint("StringContent -- insertString()");
@@ -44,7 +49,7 @@ public class removeUndo implements Testlet
         h.check("path\n", sc.getString(0, sc.length()),
                 "StringContent.insertString(): insert 'path' at 0");
         sc.insertString(0, "class");
-        UndoableEdit ue = sc.remove(1, 4);
+        ue = sc.remove(1, 4);
         java.util.Locale.setDefault(java.util.Locale.US);
         String presentationName = ue.getPresentationName();
         h.check("", presentationName, "PresentationName should be '' and is: " + presentationName);
@@ -64,10 +69,63 @@ public class removeUndo implements Testlet
         ue.redo();
         h.check("cpath\n", sc.getString(0, sc.length()),
                 "Redo: getString should be cpath\\n and is : " + sc.getString(0, sc.length()));
+        ue.die();
+        h.debug("UndoableEdit.die() no more undo/redo");
+        h.check(false, ue.canUndo(), "die, no more undo");
+        h.check(false, ue.canRedo(), "die, no more redo");
+        sc = new StringContent();
+        sc.insertString(0, "classpathX");
+        h.check("classpathX\n", sc.getString(0, sc.length()), "should be 'classpathX' and is: " + sc.getString(0, sc.length()));
+        ue = sc.remove(3, 2);
+        h.check("clapathX\n", sc.getString(0, sc.length()),
+                "double undo: should be 'clapathX\\n' and is: " + sc.getString(0, sc.length()));
+        ue2 = sc.remove(4, 2);
+        h.check("claphX\n", sc.getString(0, sc.length()),
+                "double undo: should be 'claphX\\n' and is: " + sc.getString(0, sc.length()));
+        ue3 = sc.remove(0, 3);
+        h.check(true, ue.canUndo(), "double undo can undo?");
+        h.check("phX\n", sc.getString(0, sc.length()), "check remove: should be 'phX\\n' and is: " + sc.getString(0, sc.length()));
+        ue.undo();
+        h.check("phXss\n", sc.getString(0, sc.length()),
+                "double undo: should be 'phXss\\n' and is: " + sc.getString(0, sc.length()));
+        ue2.undo();
+        h.check("phXsats\n", sc.getString(0, sc.length()), "should be 'phXsats\\n' and is: " + sc.getString(0, sc.length()));
+        ue.redo();
+        h.check("phXts\n", sc.getString(0, sc.length()), "double undo: should be 'phXts\\n' and is: " + sc.getString(0, sc.length()));
+        ue3.undo();
+        h.check("claphXts\n", sc.getString(0, sc.length()), "add an X: should be 'claphXts\\n' and is: " + sc.getString(0, sc.length()));
       }
     catch (BadLocationException ble)
       {
         h.fail("BadLocation! " + ble.getMessage());
+      }
+    try{
+      ue3.undo();
+      h.fail("should not be able to undo!");
+    }
+    catch (CannotUndoException cannot)
+      {
+        h.checkPoint("cannot undo");
+      }
+    try
+      {
+        sc = new StringContent();
+        sc.insertString(0, "super classpath");
+        h.check("super classpath\n", sc.getString(0, sc.length()), "insert 'super classpath': " + sc.getString(0, sc.length()));
+        ue = sc.remove(0, 6);
+        h.check("classpath\n", sc.getString(0, sc.length()), "insert 'super classpath': " + sc.getString(0, sc.length()));
+        ue.undo();
+        h.check("super classpath\n", sc.getString(0, sc.length()), "undo 'classpath': " + sc.getString(0, sc.length()));
+        ue.undo();
+        h.fail("should not be able to undo two times");
+      }
+    catch (BadLocationException ble)
+      {
+        h.fail("BadLocation! " + ble.getMessage());
+      }
+    catch (CannotUndoException cannot)
+      {
+        h.checkPoint("cannot undo several times");
       }
   }
 
