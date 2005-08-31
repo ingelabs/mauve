237278c11cc210c9878892e858bf5bc177ab279d
David Gilbert <david.gilbert@object-refinery.com>
2005-08-31 14:08:54 +0000

    2005-08-31  David Gilbert  <david.gilbert@object-refinery.com>
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicMenuItemUI/getPropertyPrefix.java:
    replaced with correct file.

--- gnu/testlet/javax/swing/plaf/basic/BasicMenuItemUI/getPropertyPrefix.java
+++ gnu/testlet/javax/swing/plaf/basic/BasicMenuItemUI/getPropertyPrefix.java
@@ -1,4 +1,4 @@
-// Tags: not-a-test
+// Tags: JDK1.2 
 
 // Copyright (C) 2005 David Gilbert <david.gilbert@object-refinery.com>
 
@@ -16,24 +16,30 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Mauve; see the file COPYING.  If not, write to
-// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301 USA.
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA 02110-1301 USA.
+
 
 package gnu.testlet.javax.swing.plaf.basic.BasicMenuItemUI;
 
-import javax.swing.plaf.basic.BasicMenuItemUI;
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
 
 /**
- * Provides access to protected methods.
+ * Some checks for the getPropertyPrefix() method.
  */
-public class MyBasicMenuItemUI extends BasicMenuItemUI 
+public class getPropertyPrefix implements Testlet 
 {
-  public MyBasicMenuItemUI()
+
+  /**
+   * Runs the test using the specified harness.
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness) 
   {
-    super();
+    MyBasicMenuItemUI ui = new MyBasicMenuItemUI();
+    harness.check(ui.getPropertyPrefix(), "MenuItem");
   }
-  public String getPropertyPrefix()
-  {
-    return super.getPropertyPrefix();
-  }
+  
 }
