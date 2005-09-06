a0d246f50a56469865512b78c3d543bf5d2c912d
David Gilbert <david.gilbert@object-refinery.com>
2005-09-06 20:43:09 +0000

    2005-09-06  David Gilbert  <david.gilbert@object-refinery.com>
    	* gnu/testlet/javax/swing/plaf/metal/MetalRadioButtonUI/getDisabledTextColor.java:
    new test,
    	* gnu/testlet/javax/swing/plaf/metal/MetalRadioButtonUI/getFocusColor.java: new
    test,
    	* gnu/testlet/javax/swing/plaf/metal/MetalRadioButtonUI/getSelectColor.java: new
    test,
    	* gnu/testlet/javax/swing/plaf/metal/MetalRadioButtonUI/MyMetalRadioButtonUI.java:
    support file.

---
+++ gnu/testlet/javax/swing/plaf/metal/MetalRadioButtonUI/getFocusColor.java
@@ -0,0 +1,54 @@
+// Tags: JDK1.2 
+
+// Copyright (C) 2005 David Gilbert <david.gilbert@object-refinery.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version. 
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with GNU Classpath; see the file COPYING.  If not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA.
+
+
+package gnu.testlet.javax.swing.plaf.metal.MetalRadioButtonUI;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import javax.swing.JRadioButton;
+import javax.swing.plaf.ColorUIResource;
+
+/**
+ * Some checks for the getFocusColor() method.
+ */
+public class getFocusColor implements Testlet 
+{
+
+  /**
+   * Runs the test using the specified harness.
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness) 
+  {
+    // a new instance has no select color yet
+    MyMetalRadioButtonUI ui = new MyMetalRadioButtonUI();
+    harness.check(ui.getFocusColor(), null);
+    
+    // check the setting after a call to installDefaults
+    JRadioButton b = new JRadioButton("Test");
+    ui.installDefaults(b);
+    harness.check(ui.getFocusColor(), new ColorUIResource(153, 153, 204));
+  }
+  
+}
