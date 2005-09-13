de92a223f06769ea83fdabaf3549ef769f13e225
David Gilbert <david.gilbert@object-refinery.com>
2005-09-13 19:54:43 +0000

    2005-09-13  David Gilbert  <david.gilbert@object-refinery.com>
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicCheckBoxUI/getPropertyPrefix.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicCheckBoxUI/MyBasicCheckBoxUI.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicEditorPaneUI/getPropertyPrefix.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicEditorPaneUI/MyBasicEditorPaneUI.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicFormattedTextFieldUI/getPropertyPrefix.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicFormattedTextFieldUI/MyBasicFormattedTextFieldUI.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicPasswordFieldUI/getPropertyPrefix.java:
    new test,
    	*
    gnu/testlet/javax/swing/plaf/basic/BasicPasswordFieldUI/MyBasicPasswordFieldUI.java:
    new test.
     CVS:
    ----------------------------------------------------------------------

---
+++ gnu/testlet/javax/swing/plaf/basic/BasicFormattedTextFieldUI/getPropertyPrefix.java
@@ -0,0 +1,45 @@
+// Tags: JDK1.2 
+
+// Copyright (C) 2005 David Gilbert <david.gilbert@object-refinery.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version. 
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA 02110-1301 USA.
+
+
+package gnu.testlet.javax.swing.plaf.basic.BasicFormattedTextFieldUI;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+/**
+ * Some checks for the getPropertyPrefix() method.
+ */
+public class getPropertyPrefix implements Testlet 
+{
+
+  /**
+   * Runs the test using the specified harness.
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness) 
+  {
+    MyBasicFormattedTextFieldUI ui = new MyBasicFormattedTextFieldUI();
+    harness.check(ui.getPropertyPrefix(), "FormattedTextField");
+  }
+  
+}
