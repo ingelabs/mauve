08d7dbedf326442bc16978a7041325a96bdad1e1
Anthony Balkissoon <abalkiss@redhat.com>
2005-09-15 20:54:00 +0000

    2005-09-15  Anthony Balkissoon  <abalkiss@redhat.com>
    	* gnu/testlet/java/awt/Frame/isDisplayable4.java: New test.
    	* gnu/testlet/java/awt/Frame/isDisplayable5.java: New test.

---
+++ gnu/testlet/java/awt/Frame/isDisplayable4.java
@@ -0,0 +1,55 @@
+// Tags: GUI JDK1.0
+
+// Copyright (C) 2005 Red Hat
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+package gnu.testlet.java.awt.Frame;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.awt.*;
+
+public class isDisplayable4 implements Testlet
+{
+  public void test (TestHarness harness)
+  {
+    Frame f = new Frame ();
+    Button b = new Button ();
+
+    harness.checkPoint ("before showing");
+    harness.check (f.getPeer(), null);
+    harness.check (b.getPeer(), null);
+    harness.check (f.isDisplayable (), false);
+    harness.check (b.isDisplayable (), false);
+
+    f.show ();
+
+    harness.checkPoint ("after showing frame");
+    harness.check (b.getPeer(), null);
+    harness.check (f.isDisplayable (), true);
+    harness.check (b.isDisplayable (), false);
+
+    f.add(b);
+    harness.checkPoint ("after adding button to frame");
+    harness.check (b.getParent(), f);
+    harness.check (f.isDisplayable (), true);
+    harness.check (b.isDisplayable (), true);
+  }
+}
---
+++ gnu/testlet/java/awt/Frame/isDisplayable5.java
@@ -0,0 +1,49 @@
+// Tags: GUI JDK1.0
+
+// Copyright (C) 2005 Red Hat
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+package gnu.testlet.java.awt.Frame;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.awt.*;
+
+public class isDisplayable5 implements Testlet
+{
+  public void test (TestHarness harness)
+  {
+    Frame f = new Frame ();
+    Button b = new Button ();
+    f.add(b);
+
+    harness.checkPoint ("before packing");
+    harness.check (f.getPeer(), null);
+    harness.check (b.getPeer(), null);
+    harness.check (f.isDisplayable (), false);
+    harness.check (b.isDisplayable (), false);
+
+    f.pack();
+
+    harness.checkPoint ("after packing frame");
+    harness.check (f.isDisplayable (), true);
+    harness.check (b.isDisplayable (), true);
+  }
+}
