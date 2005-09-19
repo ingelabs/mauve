63ad17d0f4a7d01cbf101fdf01d40fa22694b6c9
David Gilbert <david.gilbert@object-refinery.com>
2005-09-19 20:22:59 +0000

    2005-09-19  David Gilbert  <david.gilbert@object-refinery.com>
    	*
    gnu/testlet/javax/swing/filechooser/FileView/getDescription.java: new
    test,
    	* gnu/testlet/javax/swing/filechooser/FileView/getIcon.java: new
    test,
    	* gnu/testlet/javax/swing/filechooser/FileView/getName.java: new
    test,
    	*
    gnu/testlet/javax/swing/filechooser/FileView/getTypeDescription.java:
    new test,
    	*
    gnu/testlet/javax/swing/filechooser/FileView/isTraversable.java: new
    test,
    	* gnu/testlet/javax/swing/filechooser/FileView/MyFileView.java:
    new support class.

---
+++ gnu/testlet/javax/swing/filechooser/FileView/isTraversable.java
@@ -0,0 +1,51 @@
+// Tags: JDK1.2 
+
+// Copyright (C) 2005 David Gilbert <david.gilbert@object-refinery.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version. 
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA 02110-1301 USA.
+
+
+package gnu.testlet.javax.swing.filechooser.FileView;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileView;
+
+/**
+ * Some checks for the isTraversable() method in the {@link FileView} class.
+ */
+public class isTraversable implements Testlet 
+{
+
+  /**
+   * Runs the test using the specified harness.
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness) 
+  {
+    MyFileView fv = new MyFileView();
+    File f = new File("File1.txt");
+    harness.check(fv.isTraversable(f), null);
+    harness.check(fv.isTraversable(null), null);
+  }
+  
+}
