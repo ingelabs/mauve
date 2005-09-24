072ce9bc37cbd466691d484ee3b93f1845811db4
David Gilbert <david.gilbert@object-refinery.com>
2005-09-24 21:13:56 +0000

    2005-09-24  David Gilbert  <david.gilbert@object-refinery.com>
    	* gnu/testlet/javax/swing/DefaultComboBoxModel/addElement.java
    	(index0, index1, eventType): removed fields,
    	(events): new field,
    	(contentsChanged): use new events field,
    	(intervalAdded): likewise,
    	(intervalRemoved): likewise,
    	(test): check for multiple events.

--- gnu/testlet/javax/swing/DefaultComboBoxModel/addElement.java
+++ gnu/testlet/javax/swing/DefaultComboBoxModel/addElement.java
@@ -22,6 +22,8 @@ package gnu.testlet.javax.swing.DefaultComboBoxModel;
 import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
 
+import java.util.List;
+
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
@@ -33,29 +35,21 @@ import javax.swing.event.ListDataListener;
 public class addElement 
   implements Testlet, ListDataListener 
 {
-  int index0;
-  int index1;
-  int eventType;
+  List events = new java.util.ArrayList();
   
   public void contentsChanged(ListDataEvent event) 
   {
-    eventType = event.getType();
-    index0 = event.getIndex0();
-    index1 = event.getIndex1();
+    events.add(event);
   }
-  
+
   public void intervalAdded(ListDataEvent event) 
   {
-    eventType = event.getType();
-    index0 = event.getIndex0();
-    index1 = event.getIndex1();
+    events.add(event);
   }
   
   public void intervalRemoved(ListDataEvent event) 
   {
-    eventType = event.getType();
-    index0 = event.getIndex0();
-    index1 = event.getIndex1();
+    events.add(event);
   }
 
   /**
@@ -72,25 +66,38 @@ public class addElement
     harness.check(m.getSize(), 1);
     harness.check(m.getElementAt(0), "A");
     harness.check(m.getSelectedItem(), "A");
-    harness.check(eventType, ListDataEvent.CONTENTS_CHANGED);
-    harness.check(index0, -1);
-    harness.check(index1, -1);
+    harness.check(events.size(), 2);
+    ListDataEvent e = (ListDataEvent) events.get(0);
+    harness.check(e.getType(), ListDataEvent.INTERVAL_ADDED);
+    harness.check(e.getIndex0(), 0);
+    harness.check(e.getIndex1(), 0);
+    e = (ListDataEvent) events.get(1);
+    harness.check(e.getType(), ListDataEvent.CONTENTS_CHANGED);
+    harness.check(e.getIndex0(), -1);
+    harness.check(e.getIndex1(), -1);
+    events.clear();
     
     m.addElement("B");
     harness.check(m.getSize(), 2);
     harness.check(m.getElementAt(1), "B");
     harness.check(m.getSelectedItem(), "A");
-    harness.check(eventType, ListDataEvent.INTERVAL_ADDED);
-    harness.check(index0, 1);
-    harness.check(index1, 1);
+    harness.check(events.size(), 1);
+    e = (ListDataEvent) events.get(0);
+    harness.check(e.getType(), ListDataEvent.INTERVAL_ADDED);
+    harness.check(e.getIndex0(), 1);
+    harness.check(e.getIndex1(), 1);
+    events.clear();
     
     m.addElement(null);
     harness.check(m.getSize(), 3);
     harness.check(m.getElementAt(2), null);
     harness.check(m.getSelectedItem(), "A");
-    harness.check(eventType, ListDataEvent.INTERVAL_ADDED);
-    harness.check(index0, 2);
-    harness.check(index1, 2);
+    harness.check(events.size(), 1);
+    e = (ListDataEvent) events.get(0);
+    harness.check(e.getType(), ListDataEvent.INTERVAL_ADDED);
+    harness.check(e.getIndex0(), 2);
+    harness.check(e.getIndex1(), 2);
+    events.clear();
     
   }
 }
