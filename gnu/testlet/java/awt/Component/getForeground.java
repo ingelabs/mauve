e610f3d600a4676b0d9596e3beb53dbc93766032
Roman Kennke <roman@kennke.org>
2005-11-02 15:24:46 +0000

    2005-11-02  Roman Kennke  <kennke@aicas.com>
    	* gnu/testlet/java/awt/Component/getForeground: Modified test
    	to show that the foreground is fetched from the parent component
    	if present.

--- gnu/testlet/java/awt/Component/getForeground.java
+++ gnu/testlet/java/awt/Component/getForeground.java
@@ -22,7 +22,9 @@ package gnu.testlet.java.awt.Component;
 import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 
 public class getForeground implements Testlet {
 
@@ -39,5 +41,9 @@ public class getForeground implements Testlet {
   {
     TestComponent c = new TestComponent();
     harness.check(c.getForeground(), null);
+    Container c2 = new Container();
+    c2.setForeground(Color.GREEN);
+    c2.add(c);
+    harness.check(c.getForeground(), Color.GREEN);
   }
 }
