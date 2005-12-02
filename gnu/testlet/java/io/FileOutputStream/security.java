b4302e389435e0519115961be8bc7395758aafba
Gary Benson <gbenson@redhat.com>
2005-12-02 15:37:58 +0000

    2005-12-02  Gary Benson <gbenson@redhat.com>
    	* gnu/testlet/java/io/FileOutputStream/security.java: New test.

---
+++ gnu/testlet/java/io/FileOutputStream/security.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2005 Red Hat, Inc.
+// Written by Gary Benson <gbenson@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+package gnu.testlet.java.io.FileOutputStream;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.security.Permission;
+
+import gnu.testlet.Testlet;
+import gnu.testlet.TestHarness;
+import gnu.testlet.TestSecurityManager2;
+
+public class security implements Testlet
+{
+  public void test (TestHarness harness)
+  {
+    Permission perm = new RuntimePermission("writeFileDescriptor");
+    
+    TestSecurityManager2 sm = new TestSecurityManager2(harness);
+
+    try {
+      sm.install();
+	
+      // security: java.io.FileOutputStream-FileOutputStream(FileDescriptor)
+      harness.checkPoint("FileDescriptor constructor");
+      sm.prepareChecks(new Permission[] {perm}, new Permission[] {});
+      new FileOutputStream(FileDescriptor.out);
+      sm.checkAllChecked(harness);
+    }
+    catch (Throwable ex) {
+      harness.debug(ex);
+      harness.check(false, "Unexpected exception");
+    }
+    finally {
+	sm.uninstall();
+    }
+  }
+}
