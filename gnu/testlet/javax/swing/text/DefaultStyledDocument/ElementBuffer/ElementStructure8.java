b6cfed47718a18e956aa60d7909fbdfc67549b92
Lillian Angel <langel@redhat.com>
2006-01-16 18:20:06 +0000

    2006-01-16  Lillian Angel  <langel@redhat.com>
            * gnu/testlet/javax/swing/text/DefaultStyledDocument/ElementBuffer/ElementStructure8.java:
            Added new checks.

--- gnu/testlet/javax/swing/text/DefaultStyledDocument/ElementBuffer/ElementStructure8.java
+++ gnu/testlet/javax/swing/text/DefaultStyledDocument/ElementBuffer/ElementStructure8.java
@@ -75,6 +75,46 @@ public class ElementStructure8 implements Testlet
         Element fourth = root.getElement(3).getElement(0);
         harness.check(fourth.getStartOffset() == 65);
         harness.check(fourth.getEndOffset() == 66);
+        
+        doc = new DefaultStyledDocument();
+        root = doc.getDefaultRootElement();
+        
+        // Add a first line of text.
+        doc.insertString(0, "first line of text. \n", null);
+        harness.check(root.getElementCount() == 2);
+        harness.check(root.getElement(0).getStartOffset() == 0);
+        harness.check(root.getElement(0).getEndOffset() == 21);
+        harness.check(root.getElement(1).getStartOffset() == 21);
+        harness.check(root.getElement(1).getEndOffset() == 22);
+
+
+        // Add another line of text with 2 new lines.
+        doc.insertString(21, "\n second line of text. \n third line of text. \n", null);
+        harness.check(root.getElementCount() == 5);
+        harness.check(root.getElement(0).getElementCount() == 1);
+        harness.check(root.getElement(1).getElementCount() == 1);
+        harness.check(root.getElement(2).getElementCount() == 1);
+        harness.check(root.getElement(3).getElementCount() == 1);
+
+        first = root.getElement(0).getElement(0);
+        harness.check(first.getStartOffset() == 0);
+        harness.check(first.getEndOffset() == 21);
+
+        second = root.getElement(1).getElement(0);
+        harness.check(second.getStartOffset() == 21);
+        harness.check(second.getEndOffset() == 22);
+
+        third = root.getElement(2).getElement(0);
+        harness.check(third.getStartOffset() == 22);
+        harness.check(third.getEndOffset() == 45);
+
+        fourth = root.getElement(3).getElement(0);
+        harness.check(fourth.getStartOffset() == 45);
+        harness.check(fourth.getEndOffset() == 67);
+        
+        Element fifth = root.getElement(4).getElement(0);
+        harness.check(fifth.getStartOffset() == 67);
+        harness.check(fifth.getEndOffset() == 68);
       }
     catch (Throwable t)
       {
