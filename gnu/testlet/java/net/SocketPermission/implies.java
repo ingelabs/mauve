47fb1dbeb8c4a45bf8418ad314900e59325e1093
Gary Benson <gbenson@redhat.com>
2006-01-19 13:59:49 +0000

    2006-01-19  Gary Benson <gbenson@redhat.com>
    	* gnu/testlet/java/net/SocketPermission/implies.java: Add checks
    	for ports and (minimally) for hosts.

--- gnu/testlet/java/net/SocketPermission/implies.java
+++ gnu/testlet/java/net/SocketPermission/implies.java
@@ -27,9 +27,101 @@ import gnu.testlet.TestHarness;
 
 public class implies implements Testlet
 {
+  private Test[] hosts = new Test[] {
+    new Test("", "", true),
+    new Test("localhost", "localhost", true), // XXX need more!
+  };
+
+  private Test[] ports = new Test[] {
+    // no restriction
+    new Test("", "", true),
+    new Test("", ":80", true),
+    new Test("", ":-80", true),
+    new Test("", ":80-", true),
+    new Test("", ":70-90", true),
+    // one port
+    new Test(":80", "", false),
+    new Test(":80", ":70", false),
+    new Test(":80", ":80", true),
+    new Test(":80", ":-80", false),
+    new Test(":80", ":80-", false),
+    new Test(":80", ":70-90", false),
+    new Test(":80", ":80-80", true),
+    new Test(":80", ":90-90", false),
+    // up to and including x
+    new Test(":-80", "", false),
+    new Test(":-80", ":70", true),
+    new Test(":-80", ":80", true),
+    new Test(":-80", ":90", false),
+    new Test(":-80", ":-70", true),
+    new Test(":-80", ":-80", true),
+    new Test(":-80", ":-90", false),
+    new Test(":-80", ":70-", false),
+    new Test(":-80", ":80-", false),
+    new Test(":-80", ":90-", false),
+    new Test(":-80", ":60-70", true),
+    new Test(":-80", ":70-90", false),
+    new Test(":-80", ":90-100", false),
+    new Test(":-80", ":70-70", true),
+    new Test(":-80", ":80-80", true),
+    new Test(":-80", ":90-90", false),
+    // x and above
+    new Test(":80-", "", false),
+    new Test(":80-", ":70", false),
+    new Test(":80-", ":80", true),
+    new Test(":80-", ":90", true),
+    new Test(":80-", ":-70", false),
+    new Test(":80-", ":-80", false),
+    new Test(":80-", ":-90", false),
+    new Test(":80-", ":70-", false),
+    new Test(":80-", ":80-", true),
+    new Test(":80-", ":90-", true),
+    new Test(":80-", ":60-70", false),
+    new Test(":80-", ":70-90", false),
+    new Test(":80-", ":90-100", true),
+    new Test(":80-", ":70-70", false),
+    new Test(":80-", ":80-80", true),
+    new Test(":80-", ":90-90", true),
+    // double-ended range
+    new Test(":75-85", "", false),
+    new Test(":75-85", ":70", false),
+    new Test(":75-85", ":80", true),
+    new Test(":75-85", ":90", false),
+    new Test(":75-85", ":-70", false),
+    new Test(":75-85", ":-80", false),
+    new Test(":75-85", ":-90", false),
+    new Test(":75-85", ":70-", false),
+    new Test(":75-85", ":80-", false),
+    new Test(":75-85", ":90-", false),
+    new Test(":75-85", ":70-80", false),
+    new Test(":75-85", ":75-85", true),
+    new Test(":75-85", ":80-90", false),
+    new Test(":75-85", ":70-90", false),
+    new Test(":75-85", ":70-70", false),
+    new Test(":75-85", ":80-80", true),
+    new Test(":75-85", ":90-90", false),
+    // bit loss
+    new Test(":80", ":65616", false), // 65616 & 0xFFFF = 80
+    new Test(":80", ":-65456", false), // -65456 & 0xFFFF = 80
+    // also 4294967376?
+  };
+  
   public void test(TestHarness harness)
   {
-    harness.checkPoint("action checking");
+    harness.checkPoint("hostport checking");
+    
+    for (int i = 0; i < hosts.length; i++) {
+      for (int j = 0; j < ports.length; j++) {
+	Test test = new Test(hosts[i], ports[j]);
+	
+	SocketPermission px = new SocketPermission(test.x, "connect");
+	SocketPermission py = new SocketPermission(test.y, "connect");
+
+	harness.check(px.implies(py) == test.expect);
+      }
+    }
+
+    harness.checkPoint("actions checking");
     for (int i = 1; i < 1 << actions.length; i++) {
       for (int j = 1; j < 1 << actions.length; j++) {
 	SocketPermission pi = new SocketPermission("localhost", makeAction(i));
@@ -40,7 +132,28 @@ public class implies implements Testlet
     }
   }
 
-  // stuff for action checking
+  // stuff for hosts checking
+  private static class Test
+  {
+    String x, y;
+    boolean expect;
+
+    Test(String x, String y, boolean expect)
+    {
+      this.x = x;
+      this.y = y;
+      this.expect = expect;
+    }
+    
+    Test(Test host, Test port)
+    {
+      x = host.x + port.x;
+      y = host.y + port.y;
+      expect = host.expect && port.expect;
+    }
+  }
+
+  // stuff for actions checking
   private static String[] actions = {"accept", "connect", "listen", "resolve"};
   private static String makeAction(int mask)
   {
