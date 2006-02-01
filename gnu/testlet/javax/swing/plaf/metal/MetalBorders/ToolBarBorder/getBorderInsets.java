fb1b2d454a4f95298413f3266e6a5215e903074f
David Gilbert <david.gilbert@object-refinery.com>
2006-02-01 10:27:51 +0000

    2006-02-01  David Gilbert  <david.gilbert@object-refinery.com>
    	* gnu/testlet/javax/swing/plaf/metal/MetalBorders/MenuBarBorder/getBorderInsets.java
    	(test1): Use MetalLookAndFeel with DefaultMetalTheme,
    	(test2): Likewise,
    	* gnu/testlet/javax/swing/plaf/metal/MetalBorders/ToolBarBorder/getBorderInsets.java
    	(test1): Use MetalLookAndFeel with DefaultMetalTheme,
    	(test2): Likewise.

--- gnu/testlet/javax/swing/plaf/metal/MetalBorders/ToolBarBorder/getBorderInsets.java
+++ gnu/testlet/javax/swing/plaf/metal/MetalBorders/ToolBarBorder/getBorderInsets.java
@@ -1,6 +1,6 @@
 // Tags: JDK1.4
 
-// Copyright (C) 2005 David Gilbert <david.gilbert@object-refinery.com>
+// Copyright (C) 2005, 2006 David Gilbert <david.gilbert@object-refinery.com>
 
 // This file is part of Mauve.
 
@@ -16,8 +16,8 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Mauve; see the file COPYING.  If not, write to
-// the Free Software Foundation, 59 Temple Place - Suite 330,
-// Boston, MA 02111-1307, USA.  */
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA 02110-1301 USA.
 
 package gnu.testlet.javax.swing.plaf.metal.MetalBorders.ToolBarBorder;
 
@@ -27,6 +27,10 @@ import gnu.testlet.Testlet;
 import java.awt.Insets;
 
 import javax.swing.JToolBar;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.plaf.metal.MetalBorders.ToolBarBorder;
 
 /**
@@ -50,6 +54,16 @@ public class getBorderInsets implements Testlet
   public void test1(TestHarness harness)      
   {
     harness.checkPoint("getBorderInsets(Component)");
+    // test with DefaultMetalTheme
+    try
+      {
+        MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
+        UIManager.setLookAndFeel(new MetalLookAndFeel());
+      }
+    catch (UnsupportedLookAndFeelException e)
+      {
+        e.printStackTrace();  
+      }
     ToolBarBorder b = new ToolBarBorder();
     JToolBar tb = new JToolBar(JToolBar.HORIZONTAL);
     Insets insets1 = b.getBorderInsets(tb);
@@ -62,6 +76,16 @@ public class getBorderInsets implements Testlet
   public void test2(TestHarness harness)      
   {
     harness.checkPoint("getBorderInsets(Component, Insets)");
+    // test with DefaultMetalTheme
+    try
+      {
+        MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
+        UIManager.setLookAndFeel(new MetalLookAndFeel());
+      }
+    catch (UnsupportedLookAndFeelException e)
+      {
+        e.printStackTrace();  
+      }
     JToolBar tb = new JToolBar(JToolBar.HORIZONTAL);
     ToolBarBorder b = new ToolBarBorder();
     Insets insets = b.getBorderInsets(tb, new Insets(1, 2, 3, 4));
