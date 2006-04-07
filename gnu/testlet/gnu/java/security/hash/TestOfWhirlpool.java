049fe45174edf5308b8dd41d415f140bf5f3c1e0
Raif S. Naffah <raif@swiftdsl.com.au>
2006-04-07 23:23:07 +0000

    2006-04-08  Raif S. Naffah  <raif@swiftdsl.com.au>
    	* gnu/testlet/gnu/java/security/hash/TestOfWhirlpool.java:
    	Updated documentation.
    	(TV1): Use Version 3 test vector.
    	(TV2): Use Version 3 test vector.
    	(test): Use ISO test vectors.

--- gnu/testlet/gnu/java/security/hash/TestOfWhirlpool.java
+++ gnu/testlet/gnu/java/security/hash/TestOfWhirlpool.java
@@ -31,18 +31,28 @@ import gnu.testlet.Testlet;
 
 /**
  * Conformance tests for the {@link Whirlpool} hash implementation.
+ * <p>
+ * The test vectors used are those described in the "iso-test-vectors.txt"
+ * document included in the Whirlpool Version 3 documentation package available
+ * from
+ * <a href="http://planeta.terra.com.br/informatica/paulobarreto/WhirlpoolPage.html">
+ * The WHIRLPOOL Hashing Function</a> Paulo's page.
  */
 public class TestOfWhirlpool implements Testlet
 {
+  /** The test vector for the ASCII character 'a'. */
   private static final String TV1 =
-      "EBAA1DF2E97113BE187EB0303C660F6E643E2C090EF2CDA9A2EA6DCF5002147D"
-      + "1D0E1E9D996E879CEF9D26896630A5DB3308D5A0DC235B199C38923BE2259E03";
+      "8ACA2602792AEC6F11A67206531FB7D7F0DFF59413145E6973C45001D0087B42"
+    + "D11BC645413AEFF63A42391A39145A591A92200D560195E53B478584FDAE231A";
 
+  /** The test vector for the ASCII characters 'abc'. */
   private static final String TV2 =
-      "5777FC1F8467A1C004CD9130439403CCDAA9FDC86092D9CFFE339E6008612374"
-      + "D04C8FC0C724707FEAE6F7CEB1E030CABF652A673DA1849B02654AF76EEE24A7";
+      "4E2448A4C6F486BB16B6562C73B4020BF3043E3A731BCE721AE1B303D97E6D4C"
+    + "7181EEBDB6C57E277D0E34957114CBD6C797FC9D95D8B582D225292076D4EEF5";
 
   private IMessageDigest algorithm, clone;
+
+  // default 0-arguments constructor
 
   public void test(TestHarness harness)
   {
@@ -61,39 +71,102 @@ public class TestOfWhirlpool implements Testlet
     try
       {
         algorithm = new Whirlpool();
-        algorithm.update((byte) 0x00);
+        algorithm.update("a".getBytes(), 0, 1);
         byte[] md = algorithm.digest();
-        harness.check(TV1.equals(Util.toString(md)), "test8ZeroBits");
+        harness.check(TV1.equals(Util.toString(md)), "testA");
       }
     catch (Exception x)
       {
         harness.debug(x);
-        harness.fail("TestOfWhirlpool.test8ZeroBits");
+        harness.fail("TestOfWhirlpool.testA");
       }
 
     try
       {
         algorithm = new Whirlpool();
-        algorithm.update((byte) 0x00);
-        algorithm.update((byte) 0x00);
+        algorithm.update("abc".getBytes(), 0, 3);
         byte[] md = algorithm.digest();
-        harness.check(TV2.equals(Util.toString(md)), "test16ZeroBits");
+        harness.check(TV2.equals(Util.toString(md)), "testABC");
       }
     catch (Exception x)
       {
         harness.debug(x);
-        harness.fail("TestOfWhirlpool.test16ZeroBits");
+        harness.fail("TestOfWhirlpool.testABC");
       }
 
     try
       {
         algorithm = new Whirlpool();
-        algorithm.update((byte) 0x00);
+        algorithm.update("message digest".getBytes(), 0, 14);
+        byte[] md = algorithm.digest();
+        String exp = "378C84A4126E2DC6E56DCC7458377AAC838D00032230F53CE1F5700C0FFB4D3B"
+                   + "8421557659EF55C106B4B52AC5A4AAA692ED920052838F3362E86DBD37A8903E";
+        harness.check(exp.equals(Util.toString(md)), "testMessageDigest");
+      }
+    catch (Exception x)
+      {
+        harness.debug(x);
+        harness.fail("TestOfWhirlpool.testMessageDigest");
+      }
+
+    try
+      {
+        algorithm = new Whirlpool();
+        algorithm.update("abcdefghijklmnopqrstuvwxyz".getBytes(), 0, 26);
+        byte[] md = algorithm.digest();
+        String exp = "F1D754662636FFE92C82EBB9212A484A8D38631EAD4238F5442EE13B8054E41B"
+                   + "08BF2A9251C30B6A0B8AAE86177AB4A6F68F673E7207865D5D9819A3DBA4EB3B";
+        harness.check(exp.equals(Util.toString(md)), "testAlphabet");
+      }
+    catch (Exception x)
+      {
+        harness.debug(x);
+        harness.fail("TestOfWhirlpool.testAlphabet");
+      }
+
+    try
+      {
+        algorithm = new Whirlpool();
+        algorithm.update("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".getBytes(),
+                         0,
+                         62);
+        byte[] md = algorithm.digest();
+        String exp = "DC37E008CF9EE69BF11F00ED9ABA26901DD7C28CDEC066CC6AF42E40F82F3A1E"
+                   + "08EBA26629129D8FB7CB57211B9281A65517CC879D7B962142C65F5A7AF01467";
+        harness.check(exp.equals(Util.toString(md)), "testAsciiSubset");
+      }
+    catch (Exception x)
+      {
+        harness.debug(x);
+        harness.fail("TestOfWhirlpool.testAsciiSubset");
+      }
+
+    try
+      {
+        algorithm = new Whirlpool();
+        for (int i = 0; i < 8; i++)
+          algorithm.update("1234567890".getBytes(), 0, 10);
+        byte[] md = algorithm.digest();
+        String exp = "466EF18BABB0154D25B9D38A6414F5C08784372BCCB204D6549C4AFADB601429"
+                   + "4D5BD8DF2A6C44E538CD047B2681A51A2C60481E88C5A20B2C2A80CF3A9A083B";
+        harness.check(exp.equals(Util.toString(md)), "testEightyNumerics");
+      }
+    catch (Exception x)
+      {
+        harness.debug(x);
+        harness.fail("TestOfWhirlpool.testEightyNumerics");
+      }
+
+    try
+      {
+        algorithm = new Whirlpool();
+        algorithm.update((byte) 'a');
         clone = (IMessageDigest) algorithm.clone();
         byte[] md = algorithm.digest();
         harness.check(TV1.equals(Util.toString(md)), "testCloning #1");
 
-        clone.update((byte) 0x00);
+        clone.update((byte) 'b');
+        clone.update((byte) 'c');
         md = clone.digest();
         harness.check(TV2.equals(Util.toString(md)), "testCloning #2");
       }
