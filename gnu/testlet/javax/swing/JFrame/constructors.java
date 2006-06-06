20b333f41a3b645acae9f44967b473d52347a57b
Lillian Angel <langel@redhat.com>
2006-06-06 20:07:38 +0000

    2006-06-06  Lillian Angel  <langel@redhat.com>
            * gnu/testlet/javax/swing/JFrame/constructors.java: Fixed copyright comments.

--- gnu/testlet/javax/swing/JFrame/constructors.java
+++ gnu/testlet/javax/swing/JFrame/constructors.java
@@ -1,21 +1,23 @@
-// Tags: JDK1.2
+/* constructors.java 
+   Copyright (C) 2006 Red Hat, Tania Bento <tbento@redhat.com>
+This file is part of Mauve.
 
-// Copyright (C) 
+Mauve is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
 
-// Mauve is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2, or (at your option)
-// any later version.
+Mauve is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-// Mauve is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Mauve; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
 
-// You should have received a copy of the GNU General Public License
-// along with Mauve; see the file COPYING.  If not, write to
-// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
-// Boston, MA 02110-1301 USA.
+*/
 
 
 package gnu.testlet.javax.swing.JFrame;
