8f01047f7007080ffd48d705a31b01d1bf709c3b
David Gilbert <david.gilbert@object-refinery.com>
2006-06-22 08:46:31 +0000

    2006-06-22  David Gilbert  <david.gilbert@object-refinery.com>
    	* gnu/testlet/javax/swing/JLabel/setDisplayedMnemonic.java:
    	New test,
    	* gnu/testlet/javax/swing/JLabel/setDisplayedMnemonicIndex.java:
    	New test.

---
+++ gnu/testlet/javax/swing/JLabel/setDisplayedMnemonic.java
@@ -0,0 +1,95 @@
+/* setDisplayedMnemonic.java -- some checks for the setDisplayedMnemonic()
+       methods in the JLabel class.
+   Copyright (C) 2006 David Gilbert <david.gilbert@object-refinery.com>
+This file is part of Mauve.
+
+Mauve is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+Mauve is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mauve; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+
+*/
+
+// Tags: JDK1.4
+
+package gnu.testlet.javax.swing.JLabel;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import javax.swing.JLabel;
+
+public class setDisplayedMnemonic implements Testlet, PropertyChangeListener
+{
+
+  List events = new java.util.ArrayList();
+  
+  public void propertyChange(PropertyChangeEvent e) 
+  {
+    events.add(e);
+  }
+
+  public void test(TestHarness harness) 
+  {
+    testMethod1(harness);
+    testMethod2(harness);
+  }
+  
+  public void testMethod1(TestHarness harness) 
+  {
+    harness.checkPoint("(char)");
+    JLabel label = new JLabel("Abc Def");
+    label.addPropertyChangeListener(this);
+    label.setDisplayedMnemonic('d');
+    harness.check(label.getDisplayedMnemonic(), 68);
+    harness.check(label.getDisplayedMnemonicIndex(), 4);
+    harness.check(events.size(), 2);
+    PropertyChangeEvent e0 = (PropertyChangeEvent) events.get(0);
+    harness.check(e0.getSource(), label);
+    harness.check(e0.getPropertyName(), "displayedMnemonic");
+    harness.check(e0.getOldValue(), new Integer(0));
+    harness.check(e0.getNewValue(), new Integer(68));
+    PropertyChangeEvent e1 = (PropertyChangeEvent) events.get(1);
+    harness.check(e1.getSource(), label);
+    harness.check(e1.getPropertyName(), "displayedMnemonicIndex");
+    harness.check(e1.getOldValue(), new Integer(-1));
+    harness.check(e1.getNewValue(), new Integer(4));
+    
+    // try a character that isn't in the text
+    events.clear();
+    label.setDisplayedMnemonic('z');
+    harness.check(label.getDisplayedMnemonic(), 90);
+    harness.check(label.getDisplayedMnemonicIndex(), -1);
+    harness.check(events.size(), 2);
+    e0 = (PropertyChangeEvent) events.get(0);
+    harness.check(e0.getSource(), label);
+    harness.check(e0.getPropertyName(), "displayedMnemonic");
+    harness.check(e0.getOldValue(), new Integer(68));
+    harness.check(e0.getNewValue(), new Integer(90));
+    e1 = (PropertyChangeEvent) events.get(1);
+    harness.check(e1.getSource(), label);
+    harness.check(e1.getPropertyName(), "displayedMnemonicIndex");
+    harness.check(e1.getOldValue(), new Integer(4));
+    harness.check(e1.getNewValue(), new Integer(-1));
+  }
+
+  public void testMethod2(TestHarness harness) 
+  {
+    harness.checkPoint("(int)");
+    JLabel label = new JLabel("Abc Def");
+  }
+}
---
+++ gnu/testlet/javax/swing/JLabel/setDisplayedMnemonicIndex.java
@@ -0,0 +1,110 @@
+/* setDisplayedMnemonicIndex.java -- some checks for the 
+       setDisplayedMnemonicIndex() method.
+   Copyright (C) 2006 David Gilbert <david.gilbert@object-refinery.com>
+This file is part of Mauve.
+
+Mauve is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+Mauve is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mauve; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+
+*/
+
+// Tags: JDK1.4
+
+package gnu.testlet.javax.swing.JLabel;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import javax.swing.JLabel;
+
+public class setDisplayedMnemonicIndex 
+    implements Testlet , PropertyChangeListener
+{
+  List events = new java.util.ArrayList();
+  
+  int mnemonicIndexWhenEventFired = -1;
+  
+  public void propertyChange(PropertyChangeEvent e) 
+  {
+    events.add(e);
+    if (e.getSource() instanceof JLabel)
+      {
+        JLabel l = (JLabel) e.getSource();
+        mnemonicIndexWhenEventFired = l.getDisplayedMnemonicIndex();
+      }
+  }
+
+  public void test(TestHarness harness)
+  {
+    JLabel label = new JLabel("ABCDEFG");
+    harness.check(label.getDisplayedMnemonicIndex(), -1);
+    label.addPropertyChangeListener(this);
+    label.setDisplayedMnemonicIndex(3);
+    harness.check(label.getDisplayedMnemonicIndex(), 3);
+    harness.check(label.getDisplayedMnemonic(), 0);
+    harness.check(events.size(), 1);
+    PropertyChangeEvent e = (PropertyChangeEvent) events.get(0);
+    harness.check(e.getSource(), label);
+    harness.check(e.getPropertyName(), "displayedMnemonicIndex");
+    harness.check(e.getOldValue(), new Integer(-1));
+    harness.check(e.getNewValue(), new Integer(3));
+    harness.check(mnemonicIndexWhenEventFired, 3);  // that is, value set 
+        // before event is fired
+    
+    // setting the same value fires no event
+    events.clear();
+    label.setDisplayedMnemonicIndex(3);
+    harness.check(events.size(), 0);
+    
+    // setting the index to -1 is OK
+    events.clear();
+    label.setDisplayedMnemonicIndex(-1);
+    harness.check(label.getDisplayedMnemonicIndex(), -1);
+    harness.check(events.size(), 1);
+    e = (PropertyChangeEvent) events.get(0);
+    harness.check(e.getSource(), label);
+    harness.check(e.getOldValue(), new Integer(3));
+    harness.check(e.getNewValue(), new Integer(-1));
+    
+    // setting the index to -2 should generate an IllegalArgumentException
+    boolean pass = false;
+    try
+    {
+      label.setDisplayedMnemonicIndex(-2);
+    }
+    catch (IllegalArgumentException ex)
+    {
+      pass = true;
+    }
+    harness.check(pass);
+
+    // setting the index to the length of the label text should generate an 
+    // IllegalArgumentException
+    pass = false;
+    try
+    {
+      label.setDisplayedMnemonicIndex(label.getText().length());
+    }
+    catch (IllegalArgumentException ex)
+    {
+      pass = true;
+    }
+    harness.check(pass);
+  }
+}
