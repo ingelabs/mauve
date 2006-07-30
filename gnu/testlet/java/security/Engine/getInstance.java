ea2f488babf1e1c580ed6cb64be5279eda6d9b94
Raif S. Naffah <raif@swiftdsl.com.au>
2006-07-30 03:24:48 +0000

    2006-07-30  Raif S. Naffah  <raif@swiftdsl.com.au>
    	* gnu/testlet/java/security/Engine/getInstance.java (testNameRedundancy):
    	New method.
    	(test): Call new method.

--- gnu/testlet/java/security/Engine/getInstance.java
+++ gnu/testlet/java/security/Engine/getInstance.java
@@ -55,6 +55,7 @@ public class getInstance extends Provider implements Testlet
     setUp (harness);
     testWhiteSpace(harness);
     testAlgorithmCase (harness);
+    testNameRedundancy(harness);
   }
 
   private void setUp (TestHarness harness){
@@ -237,5 +238,93 @@ public class getInstance extends Provider implements Testlet
 
   }
 
+  /**
+   * Tests that the Provider class is immune against adding/removing the same
+   * algorithm with different case names.
+   * 
+   * @param harness the test harness.
+   */
+  private void testNameRedundancy(TestHarness harness)
+  {
+    harness.checkPoint("Engine.testNameRedundancy()");
+    try
+      {
+        mustFindName(harness, "foo");
+        mustFindName(harness, "FOO");
 
+        // add a new spelling of 'foo'
+        provider.put("MessageDigest.Foo",
+                     "gnu.testlet.java.security.MessageDigest.MauveDigest");
+        harness.verbose("*** Added 'Foo'");
+
+        mustFindName(harness, "Foo");
+
+        // now remove 'foo'.  all 'foo' spellings should not be found
+        provider.remove("MessageDigest.foo");
+        harness.verbose("*** Removed 'foo'");
+
+        mustNotFindName(harness, "foo");
+        mustNotFindName(harness, "FOO");
+        mustNotFindName(harness, "Foo");
+
+        // put 'foo' back
+        put("MessageDigest.foo",
+            "gnu.testlet.java.security.MessageDigest.MauveDigest");
+        harness.verbose("*** Re-added 'foo'");
+        // add a new spelling of 'bar'
+        put("Alg.Alias.MessageDigest.Bar", "Foo");
+        harness.verbose("*** Added alias 'Bar'");
+
+        mustFindName(harness, "bar");
+        mustFindName(harness, "BAR");
+        mustFindName(harness, "Bar");
+
+        // now remove 'bar'.  all 'bar' spellings should not be found
+        provider.remove("Alg.Alias.MessageDigest.bar");
+        harness.verbose("*** Removed alias 'bar'");
+
+        mustNotFindName(harness, "bar");
+        mustNotFindName(harness, "BAR");
+        mustNotFindName(harness, "Bar");
+      }
+    catch (Exception x)
+      {
+        harness.debug(x);
+        harness.fail("Engine.testNameRedundancy(): " + x);
+      }
+  }
+
+  private void mustFindName(TestHarness harness, String name)
+  {
+    String msg = "MUST find " + name;
+    try
+      {
+        Object obj = Engine.getInstance("MessageDigest", name, provider);
+        harness.check(obj != null, msg);
+      }
+    catch (Exception x)
+      {
+        harness.fail(msg);
+        harness.debug(x);
+      }
+  }
+
+  private void mustNotFindName(TestHarness harness, String name)
+  {
+    String msg = "MUST NOT find " + name;
+    try
+      {
+        Object obj = Engine.getInstance("MessageDigest", name, provider);
+        harness.check(obj == null, msg);
+      }
+    catch (NoSuchAlgorithmException x)
+      {
+        harness.check(true, msg);
+      }
+    catch (Exception x)
+      {
+        harness.fail(msg);
+        harness.debug(x);
+      }
+  }
 }
