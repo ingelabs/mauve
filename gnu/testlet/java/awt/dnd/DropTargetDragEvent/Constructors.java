ee60ed3f47aaf2cdfe5c04ec3b0a9e970f397b0c
Lillian Angel <langel@redhat.com>
2006-08-15 20:33:09 +0000

    2006-08-13  Lillian Angel  <langel@redhat.com>
            * gnu/testlet/java/awt/dnd/DropTargetDragEvent/Constructors.java:
            New Test.

---
+++ gnu/testlet/java/awt/dnd/DropTargetDragEvent/Constructors.java
@@ -0,0 +1,113 @@
+/* Constructors.java -- 
+   Copyright (C) 2006 Red Hat
+This file is part of Mauve.
+
+Mauve is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+Mauve is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mauve; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+
+*/
+
+package gnu.testlet.java.awt.dnd.DropTargetDragEvent;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.awt.Button;
+import java.awt.Point;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetContext;
+import java.awt.dnd.DropTargetDragEvent;
+
+public class Constructors
+    implements Testlet
+{
+  Button b = new Button("button");
+  DropTarget dt = new DropTarget(b, null);
+  DropTargetContext dtc = dt.getDropTargetContext();
+  Point loc = new Point();
+  boolean caught;
+  
+  public synchronized void test(TestHarness h)
+  {
+    testNullLoc(h);
+    testDropAction(h);
+    testSrcAction(h);
+    testNormal(h);
+  }
+  
+  void testNullLoc(TestHarness h)
+  {
+    caught = false;
+    try
+      {
+        new DropTargetDragEvent(dtc, null, DnDConstants.ACTION_COPY,
+                                DnDConstants.ACTION_COPY);
+      }
+    catch (NullPointerException npe)
+      {
+        caught = true;
+      }
+    h.check(caught);
+  }
+  
+  void testDropAction(TestHarness h)
+  {
+    caught = false;
+    try
+      {
+        new DropTargetDragEvent(dtc, loc, 4, DnDConstants.ACTION_COPY);
+      }
+    catch (IllegalArgumentException iae)
+      {
+        caught = true;
+      }
+    h.check(caught);
+  }
+  
+  void testSrcAction(TestHarness h)
+  {
+    caught = false;
+    try
+      {
+        new DropTargetDragEvent(dtc, loc, DnDConstants.ACTION_COPY, 4);
+      }
+    catch (IllegalArgumentException iae)
+      {
+        caught = true;
+      }
+    h.check(caught);
+  }
+  
+  void testNormal(TestHarness h)
+  {
+    caught = false;
+    DropTargetDragEvent dtde = null;
+    try
+      {
+        dtde = new DropTargetDragEvent(dtc, loc, DnDConstants.ACTION_COPY,
+                                DnDConstants.ACTION_COPY);
+      }
+    catch (Exception e)
+      {
+        caught = true;
+      }
+    
+    h.check(!caught);    
+    h.check(dtde != null && dtde.getDropAction() == DnDConstants.ACTION_COPY);
+    h.check(dtde != null && dtde.getSourceActions() == DnDConstants.ACTION_COPY);
+    h.check(dtde != null && dtde.getLocation().equals(loc));
+  }
+}
