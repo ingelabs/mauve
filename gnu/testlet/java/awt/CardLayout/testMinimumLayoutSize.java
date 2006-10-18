2337a099b8eda5e17c87b9f7630b423e79a19d76
Tania Bento <tbento@redhat.com>
2006-10-18 21:41:06 +0000

    2006-10-18  Tania Bento  <tbento@redhat.com>
            * gnu/testlet/java/awt/CardLayout/testMaximumLayoutSize.java: New test.
            * gnu/testlet/java/awt/CardLayout/testMinimumLayoutSize.java: New test.

---
+++ gnu/testlet/java/awt/CardLayout/testMinimumLayoutSize.java
@@ -0,0 +1,49 @@
+/* testMinimumLayoutSize.java 
+   Copyright (C) 2006 Red Hat
+This file is part of Mauve.
+
+Mauve is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+Mauve is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Mauve; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+
+*/
+
+// Tags: 1.4
+
+package gnu.testlet.java.awt.CardLayout;
+
+import java.awt.CardLayout;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+public class testMinimumLayoutSize implements Testlet
+{
+
+  public void test(TestHarness harness)
+  {
+    CardLayout layout = new CardLayout();
+    boolean failed = false;
+    try
+      {
+        layout.minimumLayoutSize(null);
+      }
+    catch (NullPointerException e)
+      {
+        failed = true;
+      }
+    harness.check(failed);
+  }
+
+}
