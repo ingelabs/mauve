cc2cf38cbbf0bfa0df2bc0993516b258d9b59cbb
Raif S. Naffah <raif@swiftdsl.com.au>
2006-11-25 23:49:52 +0000

    2006-11-26  Raif S. Naffah  <mauve@naffah-raif.name>
    	* gnu/testlet/java/util/jar/JarFile/TestOfManifest.java (ENTRYNAME): New field.
    	(test): Call checkCertificates().
    	(checkCertificates): New method.
    	(readEntries): Re-factored to use read1Entry().
    	(read1Entry): New method.

--- gnu/testlet/java/util/jar/JarFile/TestOfManifest.java
+++ gnu/testlet/java/util/jar/JarFile/TestOfManifest.java
@@ -27,6 +27,7 @@ import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.cert.Certificate;
 import java.util.Enumeration;
@@ -43,6 +44,8 @@ public class TestOfManifest
   private static final String FILENAME = "jfaceSmall.jar";
   private static final String FILEPATH = "gnu#testlet#java#util#jar#JarFile#"
       + FILENAME;
+  private static final String ENTRYNAME =
+      "org/eclipse/jface/viewers/TreeViewer$TreeColorAndFontCollector.class";
 
   /* (non-Javadoc)
    * @see gnu.testlet.Testlet#test(gnu.testlet.TestHarness)
@@ -50,6 +53,7 @@ public class TestOfManifest
   public void test(TestHarness harness)
   {
     checkManifestEntries(harness);
+    checkCertificates(harness);
   }
 
   private void checkManifestEntries(TestHarness harness)
@@ -70,35 +74,75 @@ public class TestOfManifest
       }
   }
 
+  /**
+   * @param harness this test-harness.
+   */
+  private void checkCertificates(TestHarness harness)
+  {
+    harness.checkPoint("checkCertificates");
+    try
+      {
+        File file = harness.getResourceFile(FILEPATH);
+        JarFile jarFile = new JarFile(file, true);
+        JarEntry je = jarFile.getJarEntry(ENTRYNAME);
+
+        Certificate[] certsBefore = je.getCertificates();
+        int certsBeforeCount = certsBefore == null ? 0 : certsBefore.length;
+        harness.verbose("***       before: " + certsBeforeCount);
+        harness.check(certsBeforeCount == 0, "Certificate count MUST be 0");
+
+        read1Entry(jarFile, je);
+
+        Certificate[] certsAfter = je.getCertificates();
+        int certsAfterCount = certsAfter == null ? 0 : certsAfter.length;
+        harness.verbose("***        after: " + certsAfterCount);
+        harness.check(certsAfterCount == 1, "Certificate count MUST be 1");
+        harness.check(certsBeforeCount != certsAfterCount,
+                      "Certificate counts MUST NOT be the same");
+
+        JarEntry je_ = jarFile.getJarEntry(ENTRYNAME);
+        Certificate[] sameCerts = je_.getCertificates();
+        int sameCertsCount = sameCerts == null ? 0 : sameCerts.length;
+        harness.verbose("*** w/ new entry: " + sameCertsCount);
+        harness.check(sameCertsCount == 1,
+                      "Certificate count (w/ new entry) MUST be 1");
+        harness.check(certsAfterCount == sameCertsCount,
+                      "Certificate counts (w/ new entry) MUST be the same");
+      }
+    catch (Exception x)
+      {
+        harness.debug(x);
+        harness.fail("checkCertificates: " + x);
+      }
+  }
+
   private static void readEntries(JarFile jarFile) throws Exception
   {
-    Enumeration entries = jarFile.entries();
-    while (entries.hasMoreElements())
+    for (Enumeration entries = jarFile.entries(); entries.hasMoreElements();)
+      read1Entry(jarFile, (JarEntry) entries.nextElement());
+  }
+
+  private static void read1Entry(JarFile jar, JarEntry entry) throws Exception
+  {
+    InputStream stream = null;
+    try
       {
-        JarEntry entry = (JarEntry) entries.nextElement();
-        InputStream stream = null;
-        try
-          {
-            stream = jarFile.getInputStream(entry);
-            byte[] ba = new byte[8192];
-            while (true)
-              {
-                int ret = stream.read(ba);
-                if (ret < 0)
-                  break;
-              }
-          }
-        finally
-          {
-            if (stream != null)
-              try
-                {
-                  stream.close();
-                }
-              catch (Exception ignored)
-                {
-                }
-          }
+        stream = jar.getInputStream(entry);
+        byte[] ba = new byte[8192];
+        int n;
+        while ((n = stream.read(ba)) >= 0)
+          /* keep reading */;
+      }
+    finally
+      {
+        if (stream != null)
+          try
+            {
+              stream.close();
+            }
+          catch (IOException ignored)
+            {
+            }
       }
   }
 
