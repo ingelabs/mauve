7c2a81823973989ce435b65ae4445b1e4c3b435e
Mario Torre <neugens@aicas.com>
2008-03-26 16:11:56 +0000

    2008-03-26  Mario Torre  <neugens@aicas.com>
    	* gnu/testlet/java/util/concurrent/CopyOnWriteArrayList/SubListTest.java (test):
    	new test implemented.

--- gnu/testlet/java/util/concurrent/CopyOnWriteArrayList/SubListTest.java
+++ gnu/testlet/java/util/concurrent/CopyOnWriteArrayList/SubListTest.java
@@ -23,6 +23,10 @@ Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 package gnu.testlet.java.util.concurrent.CopyOnWriteArrayList;
 
+import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
 
@@ -31,8 +35,88 @@ import gnu.testlet.Testlet;
  */
 public class SubListTest implements Testlet
 {
+  private int [] data = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19                    
+  };
+  
   public void test(TestHarness harness)
   {
+    CopyOnWriteArrayList<Integer> list =
+      new CopyOnWriteArrayList<Integer>();
+    
+    for (Integer element : data)
+      list.add(element);
+    
+    List<Integer> subList = list.subList(5, 15);
+    int i = 5;
+    for (int element : subList)
+      {
+        harness.check(element == i);
+        i++;
+      }
+    
+    // remove all the element from the subList and check the elements in the
+    // list
+    harness.checkPoint("clear list");
+    
+    subList.clear();
+    
+    harness.check(subList.size() == 0);
+    harness.check(list.size() == 10);
+    
+    i = 0;
+    for (int element : list)
+      {
+        harness.check(element == i);
+        i++;
+        if (i > 4 && i < 15)
+          i = 15;
+      }
+    
+    harness.checkPoint("ConcurrentModificationException");
+    
+    list.clear();
+    for (Integer element : data)
+      list.add(element);
+    
+    subList = list.subList(5, 15);
+    list.remove(5);
+    
+    try
+      {
+        for (int element : subList)
+          {
+            // we should never get here
+            harness.check(false);
+          }
+      }
+    catch (ConcurrentModificationException e)
+      {
+        harness.check(true);
+      }
+    
+    harness.checkPoint("Remove elements from SubList");
+    
+    list.clear();
+    for (Integer element : data)
+      list.add(element);
+    
+    subList = list.subList(5, 15);
+    
+    subList.remove(0);
+    subList.remove(0);
+    
+    harness.check(subList.size() == 8);
+    harness.check(list.size() == 18);
+    
+    subList.add(0, 6);
+    subList.add(0, 5);
+    i = 5;
+    for (int element : subList)
+      {
+        harness.check(element == i);
+        i++;
+      }
     
   }
 }
