1bd93d5b02d45e8253a9e5d27f69fd40fa26bb4c
Andrew John Hughes <gnu_andrew@member.fsf.org>
2008-07-24 17:14:15 +0000

    Correct package used for PR34580 test.
    2008-07-24  Andrew John Hughes  <gnu_andrew@member.fsf.org>
    
    	* gnu/testlet/java/awt/Desktop/PR34580.java:
    	Assign class to correct package.

--- gnu/testlet/java/awt/Desktop/PR34580.java
+++ gnu/testlet/java/awt/Desktop/PR34580.java
@@ -19,7 +19,7 @@
 // the Free Software Foundation, 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
-package gnu.testlet.javax.swing.JFrame;
+package gnu.testlet.java.awt.Desktop;
 
 import java.awt.Desktop;
 
