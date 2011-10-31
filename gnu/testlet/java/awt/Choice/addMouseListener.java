1676e2020cac895c6ae5b710e1953701983baf40
Pavel Tisnovsky <ptisnovs@redhat.com>
2011-10-31 11:29:16 +0000

    2011-10-31  Pavel Tisnovsky  <ptisnovs@redhat.com>
    	* gnu/testlet/java/awt/Choice/addComponentListener.java:
    	* gnu/testlet/java/awt/Choice/addFocusListener.java:
    	* gnu/testlet/java/awt/Choice/addHierarchyBoundsListener.java:
    	* gnu/testlet/java/awt/Choice/addHierarchyListener.java:
    	* gnu/testlet/java/awt/Choice/addInputMethodListener.java:
    	* gnu/testlet/java/awt/Choice/addItemListener.java:
    	* gnu/testlet/java/awt/Choice/addKeyListener.java:
    	* gnu/testlet/java/awt/Choice/addMouseListener.java:
    	* gnu/testlet/java/awt/Choice/addMouseMotionListener.java:
    	* gnu/testlet/java/awt/Choice/addMouseWheelListener.java:
    	Added new tests for checking if various listeners could be
    	registered for a given AWT Choice object.
    	* gnu/testlet/java/awt/Choice/list.java:
    	Added simple new test checking the method list() for a Choice object.

---
+++ gnu/testlet/java/awt/Choice/addMouseListener.java
@@ -0,0 +1,103 @@
+// addMouseListener.java -- 
+
+// Copyright (C) 2011 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: GUI
+// Uses: ../LocationTests
+
+package gnu.testlet.java.awt.Choice;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+import gnu.testlet.java.awt.LocationTests;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+  * Check if MouseListener could be registered for an AWT Choice.
+  */
+public class addMouseListener
+    implements Testlet
+{
+
+  /**
+   * Runs the test using the specified harness. 
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness)
+  {
+    Choice choice = new Choice();
+    choice.setBackground(Color.blue);
+
+    // array which will be filled by registered mouse listeners
+    MouseListener[] mouseListeners;
+
+    // get all registered mouse listeners
+    mouseListeners = choice.getMouseListeners();
+    harness.check(mouseListeners.length, 0);
+
+    // register new mouse listener
+    choice.addMouseListener(
+      new MouseListener() {
+
+        public void mouseClicked(MouseEvent e) 
+        {
+          // empty
+        }
+
+        public void mouseEntered(MouseEvent e) 
+        {
+          // empty
+        }
+
+        public void mouseExited(MouseEvent e) 
+        {
+          // empty
+        }
+
+        public void mousePressed(MouseEvent e) 
+        {
+          // empty
+        }
+
+        public void mouseReleased(MouseEvent e) 
+        {
+          // empty
+        }
+
+        @Override
+        public String toString()
+        {
+          return "myMouseListener";
+        }
+      }
+    );
+
+    // get all registered mouse listeners
+    mouseListeners = choice.getMouseListeners();
+    harness.check(mouseListeners.length, 1);
+
+    // check if the proper listener is used
+    harness.check(mouseListeners[0].toString(), "myMouseListener");
+  }
+}
+
---
+++ gnu/testlet/java/awt/Choice/addMouseMotionListener.java
@@ -0,0 +1,88 @@
+// addMouseMotionListener.java -- 
+
+// Copyright (C) 2011 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: GUI
+// Uses: ../LocationTests
+
+package gnu.testlet.java.awt.Choice;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+import gnu.testlet.java.awt.LocationTests;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+  * Check if MouseMotionListener could be registered for an AWT Choice.
+  */
+public class addMouseMotionListener
+    implements Testlet
+{
+
+  /**
+   * Runs the test using the specified harness. 
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness)
+  {
+    Choice choice = new Choice();
+    choice.setBackground(Color.blue);
+
+    // array which will be filled by registered listeners
+    MouseMotionListener[] mouseMotionListeners;
+
+    // get all registered listeners
+    mouseMotionListeners = choice.getMouseMotionListeners();
+    harness.check(mouseMotionListeners.length, 0);
+
+    // register new listener
+    choice.addMouseMotionListener(
+      new MouseMotionListener() {
+
+        public void mouseDragged(MouseEvent e)
+        {
+          // empty
+        }
+
+        public void mouseMoved(MouseEvent e)
+        {
+          // empty
+        }
+
+        @Override
+        public String toString()
+        {
+          return "myMouseMotionListener";
+        }
+      }
+    );
+
+    // get all registered listeners
+    mouseMotionListeners = choice.getMouseMotionListeners();
+    harness.check(mouseMotionListeners.length, 1);
+
+    // check if the proper listener is used
+    harness.check(mouseMotionListeners[0].toString(), "myMouseMotionListener");
+  }
+}
+
