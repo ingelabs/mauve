fe5fab2fd993a855b78cb2e6e694f443a378c904
Pavel Tisnovsky <ptisnovs@redhat.com>
2011-12-06 14:43:42 +0000

    2011-12-06  Pavel Tisnovsky  <ptisnovs@redhat.com>
            * gnu/testlet/java/awt/CardLayout/PaintTest.java:
            * gnu/testlet/java/awt/CardLayout/getLayoutAlignmentX.java:
            * gnu/testlet/java/awt/CardLayout/getLayoutAlignmentY.java:
            * gnu/testlet/java/awt/CardLayout/toString.java:
              Four new tests for checking Layout managers API:
              tests for CardLayout manager.

---
+++ gnu/testlet/java/awt/CardLayout/getLayoutAlignmentX.java
@@ -0,0 +1,61 @@
+// Tags: JDK1.1
+
+// Copyright (C) 2011 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+package gnu.testlet.java.awt.CardLayout;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.awt.CardLayout;
+import javax.swing.JComponent;
+
+/**
+ * Some checks for the getLayoutAlignmentX() method in the {@link CardLayout} class.  
+ */
+public class getLayoutAlignmentX implements Testlet
+{
+
+  /**
+   * Runs the test using the specified harness.
+   * 
+   * @param harness  the test harness (<code>null</code> not permitted).
+   */
+  public void test(TestHarness harness)
+  {
+    JComponent component = new JComponent(){};
+    CardLayout cardLayout = new CardLayout();
+
+    // Check for the value when nothing is touched.
+    harness.check(cardLayout.getLayoutAlignmentX(component), 0.5f);
+
+    // Setting the containers alignmentX doesn't change anything.
+    // 0 == alignment along the origin
+    component.setAlignmentX(0.0f);
+    harness.check(cardLayout.getLayoutAlignmentX(component), 0.5f);
+
+    // Setting the containers alignmentX doesn't change anything.
+    // 1 == alignment furthest away from the origin
+    component.setAlignmentX(1.0f);
+    harness.check(cardLayout.getLayoutAlignmentX(component), 0.5f);
+  }
+
+}
+
