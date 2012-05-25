2268a81e8c4824c54928cb2e2d4bc3499ab14d9b
Pavel Tisnovsky <ptisnovs@redhat.com>
2012-05-25 10:41:45 +0000

    * gnu/testlet/java/lang/ClassCircularityError/classInfo/InstanceOf.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/getInterfaces.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/getModifiers.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/getName.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/getPackage.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/getSimpleName.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/getSuperclass.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isAssignableFrom.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isInstance.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isInterface.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isLocalClass.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isMemberClass.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isPrimitive.java: * gnu/testlet/java/lang/ClassCircularityError/classInfo/isSynthetic.java: Added new tests for java.lang.ClassCircularityError class. * gnu/testlet/java/lang/ClassFormatError/classInfo/InstanceOf.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/getInterfaces.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/getModifiers.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/getName.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/getPackage.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/getSimpleName.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/getSuperclass.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isAssignableFrom.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isInstance.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isInterface.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isLocalClass.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isMemberClass.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isPrimitive.java: * gnu/testlet/java/lang/ClassFormatError/classInfo/isSynthetic.java: Added new tests for java.lang.ClassFormatError class.

---
+++ gnu/testlet/java/lang/ClassCircularityError/classInfo/isLocalClass.java
@@ -0,0 +1,53 @@
+// Test for method java.lang.ClassCircularityError.getClass().isLocalClass(Object)
+
+// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+package gnu.testlet.java.lang.ClassCircularityError.classInfo;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.lang.ClassCircularityError;
+
+
+
+/**
+ * Test for method java.lang.ClassCircularityError.getClass().isLocalClass()
+ */
+public class isLocalClass implements Testlet
+{
+
+    /**
+     * Runs the test using the specified harness.
+     *
+     * @param harness  the test harness (<code>null</code> not permitted).
+     */
+    public void test(TestHarness harness)
+    {
+        // create instance of a class Double
+        Object o = new ClassCircularityError("ClassCircularityError");
+
+        // get a runtime class of an object "o"
+        Class c = o.getClass();
+
+        harness.check(!c.isLocalClass());
+    }
+}
+
