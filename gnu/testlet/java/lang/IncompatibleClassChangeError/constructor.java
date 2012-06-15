0b9b6607c28b1cfbcf6df098bb77b2e2de07a1cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2012-06-15 15:25:30 +0000

    * gnu/testlet/java/lang/ExceptionInInitializerError/constructor.java: * gnu/testlet/java/lang/IllegalAccessError/constructor.java: * gnu/testlet/java/lang/IncompatibleClassChangeError/constructor.java: * gnu/testlet/java/lang/InternalError/constructor.java: * gnu/testlet/java/lang/LinkageError/constructor.java: * gnu/testlet/java/lang/NoClassDefFoundError/constructor.java: * gnu/testlet/java/lang/NoSuchFieldError/constructor.java: * gnu/testlet/java/lang/NoSuchMethodError/constructor.java: * gnu/testlet/java/lang/OutOfMemoryError/constructor.java: * gnu/testlet/java/lang/StackOverflowError/constructor.java: * gnu/testlet/java/lang/UnknownError/constructor.java: * gnu/testlet/java/lang/UnsatisfiedLinkError/constructor.java: * gnu/testlet/java/lang/UnsupportedClassVersionError/constructor.java: * gnu/testlet/java/lang/VerifyError/constructor.java: Added new constructor tests for these basic classes.

---
+++ gnu/testlet/java/lang/IncompatibleClassChangeError/constructor.java
@@ -0,0 +1,53 @@
+// Test if constructor is working properly for a class java.lang.IncompatibleClassChangeError
+
+// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+package gnu.testlet.java.lang.IncompatibleClassChangeError;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.lang.IncompatibleClassChangeError;
+
+
+
+/**
+ * Test if constructor is working properly for a class java.lang.IncompatibleClassChangeError
+ */
+public class constructor implements Testlet
+{
+
+    /**
+     * Runs the test using the specified harness.
+     *
+     * @param harness  the test harness (<code>null</code> not permitted).
+     */
+    public void test(TestHarness harness)
+    {
+        IncompatibleClassChangeError error1 = new IncompatibleClassChangeError();
+        harness.check(error1 != null);
+        harness.check(error1.toString(), "java.lang.IncompatibleClassChangeError");
+
+        IncompatibleClassChangeError error2 = new IncompatibleClassChangeError("nothing happens");
+        harness.check(error2 != null);
+        harness.check(error2.toString(), "java.lang.IncompatibleClassChangeError: nothing happens");
+    }
+}
+
