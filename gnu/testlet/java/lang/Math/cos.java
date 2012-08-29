44169260c373883cea672ae11485c8f04836afda
Pavel Tisnovsky <ptisnovs@redhat.com>
2012-08-29 14:47:21 +0000

    Added more test cases into following two tests: gnu/testlet/java/lang/Math/cos.java, gnu/testlet/java/lang/Math/sin.java.

--- gnu/testlet/java/lang/Math/cos.java
+++ gnu/testlet/java/lang/Math/cos.java
@@ -1,6 +1,7 @@
 // Tags: JDK1.0
 
 /* Copyright (C) 1999 Cygnus Solutions
+   Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
 
    This file is part of Mauve.
 
@@ -25,6 +26,126 @@ import gnu.testlet.TestHarness;
 
 public class cos implements Testlet
 {
+  /**
+   * Function (=static method) checked by this test.
+   */
+  private static double testedFunction(double input)
+    {
+      return Math.cos(input);
+    }
+
+  /**
+   * These values are used as arguments to compute cos using Math.
+   */
+  private static double[] inputValues =
+    {
+      Double.NaN,
+      Double.POSITIVE_INFINITY,
+      Double.NEGATIVE_INFINITY,
+      Double.MAX_VALUE,
+      Double.MIN_VALUE,
+      0.0,
+      Math.PI/4,
+      Math.PI/2,
+      Math.PI,
+      2*Math.PI,
+      4*Math.PI,
+      100*Math.PI,
+      1e10,
+      1e-10,
+      -0.0,
+      -Math.PI/4,
+      -Math.PI/2,
+      -Math.PI,
+      -2*Math.PI,
+      -4*Math.PI,
+      -100*Math.PI,
+      -1e10,
+      -1e-10,
+    };
+
+  /**
+   * These values are the expected results, obtained from the RI.
+   */
+  private static double[] outputValues =
+    {
+      // output value        input value
+      Double.NaN,            // NaN
+      Double.NaN,            // Infinity
+      Double.NaN,            // -Infinity
+      -0.9999876894265599,   // Double.MAX_VALUE
+      1.0,                   // Double.MIN_VALUE
+      1.0,                   // 0.0
+      0.7071067811865476,    // Math.PI/4
+      6.123233995736766E-17, // Math.PI/2
+      -1.0,                  // Math.PI
+      1.0,                   // 2*Math.PI
+      1.0,                   // 4*Math.PI
+      1.0,                   // 100*Math.PI
+      0.873119622676856,     // 1.0E10
+      1.0,                   // 1.0E-10
+      1.0,                   // -0.0
+      0.7071067811865476,    // -Math.PI/4
+      6.123233995736766E-17, // -Math.PI/2
+      -1.0,                  // -Math.PI
+      1.0,                   // -2*Math.PI
+      1.0,                   // -4*Math.PI
+      1.0,                   // -100*Math.PI
+      0.873119622676856,     // -1.0E10
+      1.0,                   // -1.0E-10
+    };
+
+  /**
+    * These values represent various NaN
+    */
+  private static long[] NaNValues =
+    {
+      0x7fff800000000000L,
+      0xffff800000000000L,
+      0x7fff812345abcdefL,
+      0xffff812345abcdefL,
+
+      0x7fff000000000001L,
+      0xffff000000000001L,
+      0x7fff7654321fedcbL,
+      0xffff7654321fedcbL
+    };
+
+  /**
+   * Test not NaN values.
+   */
+  private void testInputValues(TestHarness harness)
+  {
+    double res;
+
+    for (int i = 0; i < inputValues.length; ++i)
+      {
+	res = testedFunction(inputValues[i]);
+
+	// exact equality
+	harness.check(Double.doubleToLongBits(res), Double.doubleToLongBits(outputValues[i]));
+      }
+  }
+
+  /**
+   * Test if input NaN is returned unchanged.
+   */
+  private void testNaN(TestHarness harness)
+  {
+    long   bitsNaN;
+    double valNaN;
+
+    for (int i = 0; i < NaNValues.length; ++i)
+      {
+	bitsNaN = NaNValues[i];
+	valNaN  = Double.longBitsToDouble(bitsNaN);
+
+	// exact equality
+	harness.check(Double.doubleToRawLongBits(testedFunction(valNaN)),
+		      bitsNaN);
+      }
+  }
+
   public void test (TestHarness harness)
     {
       harness.check (new Double (Math.cos (0)).toString (), "1.0");
@@ -34,5 +155,8 @@ public class cos implements Testlet
       // It's unreasonable to expect the result of this to be eactly
       // zero, but 2^-53, the value of the constant used here, is 1ulp
       // in the range of cos.
+      testInputValues(harness);
+      testNaN(harness);
     }
+
 }
--- gnu/testlet/java/lang/Math/sin.java
+++ gnu/testlet/java/lang/Math/sin.java
@@ -23,11 +23,139 @@ package gnu.testlet.java.lang.Math;
 import gnu.testlet.Testlet;
 import gnu.testlet.TestHarness;
 
+/**
+  * Test for a static method Math.sin()
+  */
 public class sin implements Testlet
 {
+  /**
+   * Function (=static method) checked by this test.
+   */
+  private static double testedFunction(double input)
+    {
+      return Math.sin(input);
+    }
+
+  /**
+   * These values are used as arguments to compute sin using Math.
+   */
+  private static double[] inputValues =
+    {
+      Double.NaN,
+      Double.POSITIVE_INFINITY,
+      Double.NEGATIVE_INFINITY,
+      Double.MAX_VALUE,
+      Double.MIN_VALUE,
+      0.0,
+      Math.PI/4,
+      Math.PI/2,
+      Math.PI,
+      2*Math.PI,
+      4*Math.PI,
+      100*Math.PI,
+      1e10,
+      1e-10,
+      -0.0,
+      -Math.PI/4,
+      -Math.PI/2,
+      -Math.PI,
+      -2*Math.PI,
+      -4*Math.PI,
+      -100*Math.PI,
+      -1e10,
+      -1e-10,
+    };
+
+  /**
+   * These values are the expected results, obtained from the RI.
+   */
+  private static double[] outputValues =
+    {
+      // output value         input value
+      Double.NaN,             // NaN
+      Double.NaN,             // Infinity
+      Double.NaN,             // -Infinity
+      0.004961954789184062,   // Double.MAX_VALUE
+      4.9E-324,               // Double.MIN_VALUE
+      0.0,                    // 0.0
+      0.7071067811865475,     // Math.PI/4
+      1.0,                    // Math.PI/2
+      1.2246467991473532E-16, // Math.PI
+      -2.4492935982947064E-16,// 2*Math.PI
+      -4.898587196589413E-16, // 4*Math.PI
+      1.964386723728472E-15,  // 100*Math.PI
+      -0.4875060250875107,    // 1.0E10
+      1.0E-10,                // 1.0E-10
+      -0.0,                   // -0.0
+      -0.7071067811865475,    // -Math.PI/4
+      -1.0,                   // -Math.PI/2
+      -1.2246467991473532E-16,// -Math.PI
+      2.4492935982947064E-16, // -2*Math.PI
+      4.898587196589413E-16,  // -4*Math.PI
+      -1.964386723728472E-15, // -100*Math.PI
+      0.4875060250875107,     // -1.0E10
+      -1.0E-10,               // -1.0E-10
+    };
+
+  /**
+    * These values represent various NaN
+    */
+  private static long[] NaNValues =
+    {
+      0x7fff800000000000L,
+      0xffff800000000000L,
+      0x7fff812345abcdefL,
+      0xffff812345abcdefL,
+
+      0x7fff000000000001L,
+      0xffff000000000001L,
+      0x7fff7654321fedcbL,
+      0xffff7654321fedcbL
+    };
+
+  /**
+   * Test not NaN values.
+   */
+  private void testInputValues(TestHarness harness)
+  {
+    double res;
+
+    for (int i = 0; i < inputValues.length; ++i)
+      {
+	res = testedFunction(inputValues[i]);
+
+	// exact equality
+	harness.check(Double.doubleToLongBits(res), Double.doubleToLongBits(outputValues[i]));
+      }
+  }
+
+  /**
+   * Test if input NaN is returned unchanged.
+   */
+  private void testNaN(TestHarness harness)
+  {
+    long   bitsNaN;
+    double valNaN;
+
+    for (int i = 0; i < NaNValues.length; ++i)
+      {
+	bitsNaN = NaNValues[i];
+	valNaN  = Double.longBitsToDouble(bitsNaN);
+
+	// exact equality
+	harness.check(Double.doubleToRawLongBits(testedFunction(valNaN)),
+		      bitsNaN);
+      }
+  }
+
+  /**
+    * Entry point to a test.
+    */
   public void test (TestHarness harness)
   {
     harness.check (new Double (Math.sin (1e50)).toString (), 
 		   "-0.4805001434937588");
+    testInputValues(harness);
+    testNaN(harness);
   }
 }
