eb8782ca577df01a80a27c01026f9e838de47f0a
Pavel Tisnovsky <ptisnovs@redhat.com>
2013-01-16 12:42:49 +0000

    Added new tests for ArithmeticException classe.

--- gnu/testlet/java/lang/ArithmeticException/classInfo/getConstructors.java
+++ gnu/testlet/java/lang/ArithmeticException/classInfo/getConstructors.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.ArithmeticException.getClass().getConstructors()
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
