8b4dbe760aac85a84decb461df66c418f265fbce
Pavel Tisnovsky <ptisnovs@redhat.com>
2013-04-29 07:47:14 +0000

    Updated, make these tests to work correctly with OpenJDK7, added JDK tags etc.

--- gnu/testlet/java/lang/ClassFormatError/classInfo/getDeclaredFields.java
+++ gnu/testlet/java/lang/ClassFormatError/classInfo/getDeclaredFields.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.ClassFormatError.getClass().getDeclaredFields()
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -19,13 +19,16 @@
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
 
+// Tags: JDK1.5
+
 package gnu.testlet.java.lang.ClassFormatError.classInfo;
 
 import gnu.testlet.TestHarness;
 import gnu.testlet.Testlet;
 
 import java.lang.ClassFormatError;
-import java.lang.reflect.Modifier;
+import java.util.Map;
+import java.util.HashMap;
 
 
 
@@ -42,22 +45,58 @@ public class getDeclaredFields implements Testlet
      */
     public void test(TestHarness harness)
     {
-        String[] fieldNames = new String[] {
-        };
-        java.util.Arrays.sort(fieldNames);
+        // map of declared fields which should exists
+        Map<String, String> testedDeclaredFields = null;
 
-        String[] fieldStrings = new String[] {
-        };
-        java.util.Arrays.sort(fieldStrings);
+        // map of declared fields for (Open)JDK6
+        Map<String, String> testedDeclaredFields_jdk6 = new HashMap<String, String>();
+
+        // map of declared fields for (Open)JDK7
+        Map<String, String> testedDeclaredFields_jdk7 = new HashMap<String, String>();
+
+        // map for fields declared in (Open)JDK6
+        // --- empty ---
+
+        // map for fields declared in (Open)JDK7
+        testedDeclaredFields_jdk7.put("private static final long java.lang.ClassFormatError.serialVersionUID", "serialVersionUID");
 
         // create instance of a class ClassFormatError
-        Object o = new ClassFormatError("ClassFormatError");
+        final Object o = new ClassFormatError("ClassFormatError");
 
         // get a runtime class of an object "o"
-        Class c = o.getClass();
+        final Class c = o.getClass();
 
-        java.lang.reflect.Field[] fields = c.getDeclaredFields();
-        harness.check(fields.length, 0);
+        // get the right map containing declared field signatures
+        testedDeclaredFields = getJavaVersion() < 7 ? testedDeclaredFields_jdk6 : testedDeclaredFields_jdk7;
+
+        // get all declared fields for this class
+        java.lang.reflect.Field[] declaredFields = c.getDeclaredFields();
+
+        // expected number of declared fields
+        final int expectedNumberOfDeclaredFields = testedDeclaredFields.size();
+
+        // basic check for a number of declared fields
+        harness.check(declaredFields.length, expectedNumberOfDeclaredFields);
+
+        // check if all fields exist
+        for (java.lang.reflect.Field declaredField: declaredFields) {
+            String fieldName = declaredField.getName();
+            String fieldString = declaredField.toString();
+            harness.check(testedDeclaredFields.containsKey(fieldString));
+            harness.check(testedDeclaredFields.get(fieldString), fieldName);
+        }
+    }
+
+    /**
+     * Returns version of Java. The input could have the following form: "1.7.0_06"
+     * and we are interested only in "7" in this case.
+     * 
+     * @return Java version
+     */
+    protected int getJavaVersion() {
+        String javaVersionStr = System.getProperty("java.version");
+        String[] parts = javaVersionStr.split("\\.");
+        return Integer.parseInt(parts[1]);
     }
 }
 
