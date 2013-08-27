4dbf46aa4bcff6d58cb86718704ad7143ad99cc0
Pavel Tisnovsky <ptisnovs@redhat.com>
2013-08-27 08:42:51 +0000

    Updated, make these tests to work correctly with OpenJDK7, added JDK tags etc.

--- gnu/testlet/java/lang/IllegalAccessException/classInfo/getPackage.java
+++ gnu/testlet/java/lang/IllegalAccessException/classInfo/getPackage.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.IllegalAccessException.getClass().getPackage()
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -18,6 +18,8 @@
 // along with Mauve; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
 
 package gnu.testlet.java.lang.IllegalAccessException.classInfo;
 
@@ -41,11 +43,11 @@ public class getPackage implements Testlet
      */
     public void test(TestHarness harness)
     {
-        // create instance of a class Double
-        Object o = new IllegalAccessException("IllegalAccessException");
+        // create instance of a class IllegalAccessException
+        final Object o = new IllegalAccessException("java.lang.IllegalAccessException");
 
         // get a runtime class of an object "o"
-        Class c = o.getClass();
+        final Class c = o.getClass();
 
         Package p = c.getPackage();
         harness.check(p.getName(), "java.lang");
