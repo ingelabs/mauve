544694a0e107c1de9f29121a21e1052ed80495b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2013-09-11 07:54:42 +0000

    Updated, make these tests to work correctly with OpenJDK7, added JDK tags etc.

--- gnu/testlet/java/lang/IllegalMonitorStateException/classInfo/InstanceOf.java
+++ gnu/testlet/java/lang/IllegalMonitorStateException/classInfo/InstanceOf.java
@@ -1,6 +1,6 @@
 // Test for instanceof operator applied to a class java.lang.IllegalMonitorStateException
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -18,6 +18,8 @@
 // along with Mauve; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
 
 package gnu.testlet.java.lang.IllegalMonitorStateException.classInfo;
 
@@ -46,7 +48,7 @@ public class InstanceOf implements Testlet
     public void test(TestHarness harness)
     {
         // create instance of a class IllegalMonitorStateException
-        IllegalMonitorStateException o = new IllegalMonitorStateException("IllegalMonitorStateException");
+        IllegalMonitorStateException o = new IllegalMonitorStateException("java.lang.IllegalMonitorStateException");
 
         // basic check of instanceof operator
         harness.check(o instanceof IllegalMonitorStateException);
