9caa9054907cb8265f7e37ba822ad3bc1936d0bd
Pavel Tisnovsky <ptisnovs@redhat.com>
2013-10-04 08:38:54 +0000

    Updated, make these tests to work correctly with OpenJDK7, added JDK tags etc.

--- gnu/testlet/java/lang/IllegalThreadStateException/classInfo/InstanceOf.java
+++ gnu/testlet/java/lang/IllegalThreadStateException/classInfo/InstanceOf.java
@@ -1,6 +1,6 @@
 // Test for instanceof operator applied to a class java.lang.IllegalThreadStateException
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -18,6 +18,8 @@
 // along with Mauve; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
 
 package gnu.testlet.java.lang.IllegalThreadStateException.classInfo;
 
@@ -47,7 +49,7 @@ public class InstanceOf implements Testlet
     public void test(TestHarness harness)
     {
         // create instance of a class IllegalThreadStateException
-        IllegalThreadStateException o = new IllegalThreadStateException("IllegalThreadStateException");
+        IllegalThreadStateException o = new IllegalThreadStateException("java.lang.IllegalThreadStateException");
 
         // basic check of instanceof operator
         harness.check(o instanceof IllegalThreadStateException);
