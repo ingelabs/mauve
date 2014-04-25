dd980739b7564b1d8e3cebc18e66897048ba387a
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-04-25 10:22:51 +0000

    Updated (c) year.

--- gnu/testlet/java/lang/NoSuchMethodException/classInfo/getComponentType.java
+++ gnu/testlet/java/lang/NoSuchMethodException/classInfo/getComponentType.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.NoSuchMethodException.getClass().getComponentType()
 
-// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013, 2014 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
