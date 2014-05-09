4b7496c5117096e2e60b4f3e50e2653a3b51b0a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2014-05-09 08:35:53 +0000

    Updated existing tests, make these tests to work correctly with OpenJDK7, added JDK tags etc.

--- gnu/testlet/java/lang/NullPointerException/classInfo/getDeclaredConstructor.java
+++ gnu/testlet/java/lang/NullPointerException/classInfo/getDeclaredConstructor.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.NullPointerException.getClass().getDeclaredConstructor()
 
-// Copyright (C) 2012, 2013 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013, 2014 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
