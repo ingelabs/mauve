442c2b3b43333015c0e0f9eec1d46e834c53ab03
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-04-07 10:03:04 +0000

    Updated to work correctly in JDK6.

--- gnu/testlet/java/lang/Class/classInfo/getDeclaredFields.java
+++ gnu/testlet/java/lang/Class/classInfo/getDeclaredFields.java
@@ -51,6 +51,37 @@ public class getDeclaredFields implements Testlet
         // map of declared fields for (Open)JDK6
         Map<String, String> testedDeclaredFields_jdk6 = new HashMap<String, String>();
 
+        // map for fields declared in (Open)JDK6
+        testedDeclaredFields_jdk6.put("private static final int java.lang.Class.ANNOTATION", "ANNOTATION");
+        testedDeclaredFields_jdk6.put("private static final int java.lang.Class.ENUM", "ENUM");
+        testedDeclaredFields_jdk6.put("private static final int java.lang.Class.SYNTHETIC", "SYNTHETIC");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.reflect.Constructor java.lang.Class.cachedConstructor", "cachedConstructor");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.Class java.lang.Class.newInstanceCallerCache", "newInstanceCallerCache");
+        testedDeclaredFields_jdk6.put("private transient java.lang.String java.lang.Class.name", "name");
+        testedDeclaredFields_jdk6.put("private static java.security.ProtectionDomain java.lang.Class.allPermDomain", "allPermDomain");
+        testedDeclaredFields_jdk6.put("private static boolean java.lang.Class.useCaches", "useCaches");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.declaredFields", "declaredFields");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.publicFields", "publicFields");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.declaredMethods", "declaredMethods");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.publicMethods", "publicMethods");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.declaredConstructors", "declaredConstructors");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.publicConstructors", "publicConstructors");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.declaredPublicFields", "declaredPublicFields");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.ref.SoftReference java.lang.Class.declaredPublicMethods", "declaredPublicMethods");
+        testedDeclaredFields_jdk6.put("private transient volatile int java.lang.Class.classRedefinedCount", "classRedefinedCount");
+        testedDeclaredFields_jdk6.put("private transient volatile int java.lang.Class.lastRedefinedCount", "lastRedefinedCount");
+        testedDeclaredFields_jdk6.put("private transient sun.reflect.generics.repository.ClassRepository java.lang.Class.genericInfo", "genericInfo");
+        testedDeclaredFields_jdk6.put("private static final long java.lang.Class.serialVersionUID", "serialVersionUID");
+        testedDeclaredFields_jdk6.put("private static final java.io.ObjectStreamField[] java.lang.Class.serialPersistentFields", "serialPersistentFields");
+        testedDeclaredFields_jdk6.put("private static sun.reflect.ReflectionFactory java.lang.Class.reflectionFactory", "reflectionFactory");
+        testedDeclaredFields_jdk6.put("private static boolean java.lang.Class.initted", "initted");
+        testedDeclaredFields_jdk6.put("private transient volatile java.lang.Object[] java.lang.Class.enumConstants", "enumConstants");
+        testedDeclaredFields_jdk6.put("private transient volatile java.util.Map java.lang.Class.enumConstantDirectory", "enumConstantDirectory");
+        testedDeclaredFields_jdk6.put("private transient java.util.Map java.lang.Class.annotations", "annotations");
+        testedDeclaredFields_jdk6.put("private transient java.util.Map java.lang.Class.declaredAnnotations", "declaredAnnotations");
+        testedDeclaredFields_jdk6.put("private sun.reflect.annotation.AnnotationType java.lang.Class.annotationType", "annotationType");
+        testedDeclaredFields_jdk6.put("private static java.lang.annotation.Annotation[] java.lang.Class.EMPTY_ANNOTATIONS_ARRAY", "EMPTY_ANNOTATIONS_ARRAY");
+
         // map of declared fields for (Open)JDK7
         Map<String, String> testedDeclaredFields_jdk7 = new HashMap<String, String>();
 
