56c6358f5721dd7a913daa4a747d8273ece346bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-05-14 09:06:26 +0000

    Updated six tests in java.lang.Float/classInfo.

--- gnu/testlet/java/lang/Float/classInfo/isAnonymousClass.java
+++ gnu/testlet/java/lang/Float/classInfo/isAnonymousClass.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.Float.getClass().isAnonymousClass()
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013, 2014, 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -18,6 +18,8 @@
 // along with Mauve; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
 
 package gnu.testlet.java.lang.Float.classInfo;
 
@@ -42,10 +44,10 @@ public class isAnonymousClass implements Testlet
     public void test(TestHarness harness)
     {
         // create instance of a class Float
-        Object o = new Float(42.0f);
+        final Object o = new Float(42.0f);
 
         // get a runtime class of an object "o"
-        Class c = o.getClass();
+        final Class c = o.getClass();
 
         harness.check(!c.isAnonymousClass());
     }
