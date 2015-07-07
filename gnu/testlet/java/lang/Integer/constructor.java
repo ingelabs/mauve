59d38751348e6541051153a52bcf5d1edc98464b
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-07-07 10:29:30 +0000

    Added new test: java.lang.Integer.constructor.

---
+++ gnu/testlet/java/lang/Integer/constructor.java
@@ -0,0 +1,68 @@
+// Test if instances of a class java.lang.Integer could be properly constructed
+
+// Copyright (C) 2012, 2013, 2014, 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
+
+package gnu.testlet.java.lang.Integer;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.lang.Integer;
+
+
+
+/**
+ * Test if instances of a class java.lang.Integer
+ * could be properly constructed
+ */
+public class constructor implements Testlet
+{
+
+    /**
+     * Runs the test using the specified harness.
+     *
+     * @param harness  the test harness (<code>null</code> not permitted).
+     */
+    public void test(TestHarness harness)
+    {
+        Integer object1 = new Integer(42);
+        harness.check(object1 != null);
+        harness.check(object1.toString(), "42");
+
+        Integer object2 = new Integer("42");
+        harness.check(object2 != null);
+        harness.check(object2.toString(), "42");
+
+        Integer object3 = new Integer("0");
+        harness.check(object3 != null);
+        harness.check(object3.toString(), "0");
+
+        Integer object4 = new Integer(Integer.MAX_VALUE);
+        harness.check(object4 != null);
+        harness.check(object4.toString(), "2147483647");
+
+        Integer object5 = new Integer(Integer.MIN_VALUE);
+        harness.check(object5 != null);
+        harness.check(object5.toString(), "-2147483648");
+    }
+}
+
