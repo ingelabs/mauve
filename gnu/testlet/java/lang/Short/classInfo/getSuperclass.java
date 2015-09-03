5e37734f81edb2fbd055aaa1323fa131e78ba245
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-03 08:59:36 +0000

    Updated seven tests in java.lang.Short/classInfo.

--- gnu/testlet/java/lang/Short/classInfo/getSuperclass.java
+++ gnu/testlet/java/lang/Short/classInfo/getSuperclass.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.Short.getClass().getSuperclass()
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013, 2014, 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -18,6 +18,8 @@
 // along with Mauve; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
 
 package gnu.testlet.java.lang.Short.classInfo;
 
@@ -41,11 +43,11 @@ public class getSuperclass implements Testlet
      */
     public void test(TestHarness harness)
     {
-        // create instance of a class Double
-        Object o = new Short((short)42);
+        // create instance of a class Short
+        final Object o = new Short("42");
 
         // get a runtime class of an object "o"
-        Class c = o.getClass();
+        final Class c = o.getClass();
 
         Class superClass = c.getSuperclass();
         harness.check(superClass.getName(), "java.lang.Number");
