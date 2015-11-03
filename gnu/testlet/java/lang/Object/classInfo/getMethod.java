443c2f4fb2f2a79b575a3e0599d848a033edea95
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-03 12:35:45 +0000

    Updated seven tests in: java/lang/Object/classInfo/.

--- gnu/testlet/java/lang/Object/classInfo/getMethod.java
+++ gnu/testlet/java/lang/Object/classInfo/getMethod.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.Object.getClass().getMethod()
 
-// Copyright (C) 2012, 2013, 2014 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013, 2014, 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
