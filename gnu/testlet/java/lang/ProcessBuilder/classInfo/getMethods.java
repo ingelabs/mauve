ef42ca5483727651980afe501d8f680f400d2b23
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-10 13:12:46 +0000

    Added six new tests into ProcessBuilder/classInfo.

---
+++ gnu/testlet/java/lang/ProcessBuilder/classInfo/getMethods.java
@@ -0,0 +1,148 @@
+// Test for method java.lang.ProcessBuilder.getClass().getMethods()
+
+// Copyright (C) 2012, 2013, 2014, 2015 Pavel Tisnovsky <ptisnovs@redhat.com>
+
+// This file is part of Mauve.
+
+// Mauve is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// Mauve is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Mauve; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
+
+package gnu.testlet.java.lang.ProcessBuilder.classInfo;
+
+import gnu.testlet.TestHarness;
+import gnu.testlet.Testlet;
+
+import java.lang.ProcessBuilder;
+import java.util.Map;
+import java.util.HashMap;
+
+
+
+/**
+ * Test for method java.lang.ProcessBuilder.getClass().getMethods()
+ */
+public class getMethods implements Testlet
+{
+
+    /**
+     * Runs the test using the specified harness.
+     *
+     * @param harness  the test harness (<code>null</code> not permitted).
+     */
+    public void test(TestHarness harness)
+    {
+        // map of methods which should exists
+        Map<String, String> testedMethods = null;
+
+        // map of methods for (Open)JDK6
+        Map<String, String> testedMethods_jdk6 = new HashMap<String, String>();
+
+        // map of methods for (Open)JDK7
+        Map<String, String> testedMethods_jdk7 = new HashMap<String, String>();
+
+        // map for methods declared in (Open)JDK6
+        testedMethods_jdk6.put("public java.lang.Process java.lang.ProcessBuilder.start() throws java.io.IOException", "start");
+        testedMethods_jdk6.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.command(java.util.List)", "command");
+        testedMethods_jdk6.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.command(java.lang.String[])", "command");
+        testedMethods_jdk6.put("public java.util.List java.lang.ProcessBuilder.command()", "command");
+        testedMethods_jdk6.put("public java.util.Map java.lang.ProcessBuilder.environment()", "environment");
+        testedMethods_jdk6.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.directory(java.io.File)", "directory");
+        testedMethods_jdk6.put("public java.io.File java.lang.ProcessBuilder.directory()", "directory");
+        testedMethods_jdk6.put("public boolean java.lang.ProcessBuilder.redirectErrorStream()", "redirectErrorStream");
+        testedMethods_jdk6.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectErrorStream(boolean)", "redirectErrorStream");
+        testedMethods_jdk6.put("public final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException", "wait");
+        testedMethods_jdk6.put("public final void java.lang.Object.wait(long) throws java.lang.InterruptedException", "wait");
+        testedMethods_jdk6.put("public final void java.lang.Object.wait() throws java.lang.InterruptedException", "wait");
+        testedMethods_jdk6.put("public boolean java.lang.Object.equals(java.lang.Object)", "equals");
+        testedMethods_jdk6.put("public java.lang.String java.lang.Object.toString()", "toString");
+        testedMethods_jdk6.put("public int java.lang.Object.hashCode()", "hashCode");
+        testedMethods_jdk6.put("public final java.lang.Class java.lang.Object.getClass()", "getClass");
+        testedMethods_jdk6.put("public final void java.lang.Object.notify()", "notify");
+        testedMethods_jdk6.put("public final void java.lang.Object.notifyAll()", "notifyAll");
+
+        // map for methods declared in (Open)JDK7
+        testedMethods_jdk7.put("public java.lang.Process java.lang.ProcessBuilder.start() throws java.io.IOException", "start");
+        testedMethods_jdk7.put("public java.io.File java.lang.ProcessBuilder.directory()", "directory");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.directory(java.io.File)", "directory");
+        testedMethods_jdk7.put("public java.util.Map java.lang.ProcessBuilder.environment()", "environment");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.command(java.util.List)", "command");
+        testedMethods_jdk7.put("public java.util.List java.lang.ProcessBuilder.command()", "command");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.command(java.lang.String[])", "command");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.inheritIO()", "inheritIO");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectError(java.io.File)", "redirectError");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder$Redirect java.lang.ProcessBuilder.redirectError()", "redirectError");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectError(java.lang.ProcessBuilder$Redirect)", "redirectError");
+        testedMethods_jdk7.put("public boolean java.lang.ProcessBuilder.redirectErrorStream()", "redirectErrorStream");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectErrorStream(boolean)", "redirectErrorStream");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectInput(java.io.File)", "redirectInput");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder$Redirect java.lang.ProcessBuilder.redirectInput()", "redirectInput");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectInput(java.lang.ProcessBuilder$Redirect)", "redirectInput");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectOutput(java.io.File)", "redirectOutput");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder java.lang.ProcessBuilder.redirectOutput(java.lang.ProcessBuilder$Redirect)", "redirectOutput");
+        testedMethods_jdk7.put("public java.lang.ProcessBuilder$Redirect java.lang.ProcessBuilder.redirectOutput()", "redirectOutput");
+        testedMethods_jdk7.put("public final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException", "wait");
+        testedMethods_jdk7.put("public final void java.lang.Object.wait(long) throws java.lang.InterruptedException", "wait");
+        testedMethods_jdk7.put("public final void java.lang.Object.wait() throws java.lang.InterruptedException", "wait");
+        testedMethods_jdk7.put("public boolean java.lang.Object.equals(java.lang.Object)", "equals");
+        testedMethods_jdk7.put("public java.lang.String java.lang.Object.toString()", "toString");
+        testedMethods_jdk7.put("public int java.lang.Object.hashCode()", "hashCode");
+        testedMethods_jdk7.put("public final java.lang.Class java.lang.Object.getClass()", "getClass");
+        testedMethods_jdk7.put("public final void java.lang.Object.notify()", "notify");
+        testedMethods_jdk7.put("public final void java.lang.Object.notifyAll()", "notifyAll");
+
+        // create instance of a class ProcessBuilder
+        final Object o = new ProcessBuilder("");
+
+        // get a runtime class of an object "o"
+        final Class c = o.getClass();
+
+        // get the right map containing method signatures
+        testedMethods = getJavaVersion() < 7 ? testedMethods_jdk6 : testedMethods_jdk7;
+
+        // get all methods for this class
+        java.lang.reflect.Method[] methods = c.getMethods();
+
+        // expected number of methods
+        final int expectedNumberOfMethods = testedMethods.size();
+
+        // basic check for a number of methods
+        harness.check(methods.length, expectedNumberOfMethods);
+
+        // check if all methods exist
+        for (java.lang.reflect.Method method : methods) {
+            // method name should consists of package name + class name
+            String methodName = method.getName();
+            // modifiers + package + method name + parameter types
+            String methodString = method.toString().replaceAll(" native ", " ");
+            harness.check(testedMethods.containsKey(methodString));
+            harness.check(testedMethods.get(methodString), methodName);
+        }
+    }
+
+    /**
+     * Returns version of Java. The input could have the following form: "1.7.0_06"
+     * and we are interested only in "7" in this case.
+     * 
+     * @return Java version
+     */
+    protected int getJavaVersion() {
+        String javaVersionStr = System.getProperty("java.version");
+        String[] parts = javaVersionStr.split("\\.");
+        return Integer.parseInt(parts[1]);
+    }
+}
+
