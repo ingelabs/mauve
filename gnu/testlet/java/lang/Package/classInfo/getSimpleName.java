41eeaccdbea926c51dfe69862fc263417c1c9270
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-21 10:36:46 +0000

    Updated.

--- gnu/testlet/java/lang/Package/classInfo/getSimpleName.java
+++ gnu/testlet/java/lang/Package/classInfo/getSimpleName.java
@@ -1,6 +1,6 @@
 // Test for method java.lang.Package.getClass().getSimpleName()
 
-// Copyright (C) 2012 Pavel Tisnovsky <ptisnovs@redhat.com>
+// Copyright (C) 2012, 2013, 2014, 2015, 2016 Pavel Tisnovsky <ptisnovs@redhat.com>
 
 // This file is part of Mauve.
 
@@ -18,6 +18,8 @@
 // along with Mauve; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Fifth Floor, Boston, MA 02110-1301 USA.
+
+// Tags: JDK1.5
 
 package gnu.testlet.java.lang.Package.classInfo;
 
@@ -41,11 +43,11 @@ public class getSimpleName implements Testlet
      */
     public void test(TestHarness harness)
     {
-        // create instance of a class Double
-        Object o = Package.getPackage("java.lang");
+        // create instance of a class Package
+        final Object o = Package.getPackage("java.lang");
 
         // get a runtime class of an object "o"
-        Class c = o.getClass();
+        final Class c = o.getClass();
 
         harness.check(c.getSimpleName(), "Package");
     }
